2ddbd3a718984352673f69bb2762db74207f726a
Cole Gentry <Peapod2007@gmail.com>
2016-10-25 22:16:03 -0400

    refactor(Complete refactor of the app to reflect angular 2 build process):
    undefined

--- .eslintrc.js
+++ .eslintrc.js
@@ -113,32 +113,7 @@ module.exports = {
     'node': true,
     'browser': true,
   },
-  "ecmaFeatures": {
-    "arrowFunctions": true,
-    "binaryLiterals": true,
-    "blockBindings": true,
-    "classes": true,
-    "defaultParams": true,
-    "destructuring": true,
-    "forOf": true,
-    "generators": true,
-    "modules": true,
-    "objectLiteralComputedProperties": true,
-    "objectLiteralDuplicateProperties": true,
-    "objectLiteralShorthandMethods": true,
-    "objectLiteralShorthandProperties": true,
-    "octalLiterals": true,
-    "regexUFlag": true,
-    "regexYFlag": true,
-    "spread": true,
-    "superInFunctions": true,
-    "templateStrings": true,
-    "unicodeCodePointEscapes": true,
-    "globalReturn": true,
-    "jsx": true
-  },
-  "plugins": ["react"],
   // Include all the recommended rules, most for error-avoidance and best
   // practices
-  'extends': 'airbnb',
+  'extends': 'airbnb-base',
 };
--- gulpfile.js
+++ gulpfile.js
@@ -3,12 +3,8 @@ const gulp = require('gulp');
 
 // Include Our Plugins
 const imagemin = require('gulp-imagemin');
-const babel = require('gulp-babel');
-const eslint = require('gulp-eslint');
 const cleanCSS = require('gulp-clean-css');
 const browserSync = require('browser-sync').create();
-
-const reload = browserSync.reload;
 const jsonminify = require('gulp-jsonminify');
 const autoprefixer = require('gulp-autoprefixer');
 const minifyHTML = require('gulp-minify-html');
@@ -21,20 +17,7 @@ const del = require('del');
 /* Cleans Directories ------------------------------------------------------- */
 gulp.task('clean', () => del([
   'dist/**',
-  'babel/**',
 ]));
-
-/* Lint Task ---------------------------------------------------------------- */
-gulp.task('lint', () =>
-  gulp.src('src/js/**/*.jsx')
-    .pipe(eslint({
-      baseConfig: {
-        'ecmaFeatures': {
-          'jsx': true,
-        },
-      },
-    }))
-    .pipe(eslint.format()));
 
 /* HTML Minification -------------------------------------------------------- */
 gulp.task('html', () => {
@@ -63,26 +46,19 @@ gulp.task('sass', () =>
       .pipe(cleanCSS())
       .pipe(gulp.dest('dist/css'))
   ));
-/* Babel Transpiling -------------------------------------------------------- */
-gulp.task('babel', () =>
-  gulp.src('src/js/**/*.{jsx,js}')
-    .pipe(babel({
-      presets: ['react'] }))
-    .pipe(gulp.dest('babel')));
 /* Concatenate & Minify JS -------------------------------------------------- */
 gulp.task('scripts', () => (
     gulp.src(['src/js/data/*.json'])
       .pipe(jsonminify())
       .pipe(gulp.dest('dist/js/data')),
-    gulp.src([
-      // List of Library Files
-      'node_modules/react/dist/react.min.js',
-      'node_modules/react-dom/dist/react-dom.min.js',
-    ])
-      .pipe(concat('lib.js'))
-      .pipe(rename({ suffix: '.min' }))
-      .pipe(gulp.dest('dist/js')),
-    gulp.src('babel/**/*.js')
+    // gulp.src([
+    //   // List of Library Files
+    //   ''
+    // ])
+    //   .pipe(concat('lib.js'))
+    //   .pipe(rename({ suffix: '.min' }))
+    //   .pipe(gulp.dest('dist/js')),
+    gulp.src('src/js/**/*.js')
       .pipe(concat('app.js'))
       .pipe(rename({ suffix: '.min' }))
       .pipe(uglify())
@@ -101,20 +77,21 @@ gulp.task('serve', () => {
   browserSync.init({
     server: {
       baseDir: 'dist',
+      files: ['dist/**/*'],
     },
   });
+  browserSync.watch(['dist/**/*']);
   /* Watch Files For Changes ------------------------------------------------ */
   gulp.watch('src/scss/**/*.scss', gulp.series('sass'));
-  gulp.watch('assets/js/src/**/*.jsx', gulp.series('lint', 'babel', 'scripts'));
+  gulp.watch('src/js/**/*.js', gulp.series('scripts'));
   gulp.watch('src/assets/**/*', gulp.series('images'));
-  gulp.watch('src/**/*.html', gulp.series('html', 'scripts'));
-  gulp.watch(['dist/css/**/*.css', 'dist/*.html', 'dist/assets/**/*', 'dist/js/**/*.js'], reload);
+  gulp.watch('src/**/*.html', gulp.series('html'));
 });
 
 /* Default Task ------------------------------------------------------------- */
-gulp.task('default', gulp.series('clean', 'lint', 'babel', 'images',
+gulp.task('default', gulp.series('clean', 'images',
   gulp.parallel('html', 'sass', 'scripts'), 'serve'));
 
 /* Distribution Prep Task --------------------------------------------------- */
-gulp.task('dist', gulp.series('clean', 'lint', 'babel', 'images',
+gulp.task('dist', gulp.series('clean', 'images',
   gulp.parallel('html', 'sass', 'scripts')));
