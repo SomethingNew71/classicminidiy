579419a1dedb1b5d0a8a2f106d8561313883cf4f
Cole Gentry <Peapod2007@gmail.com>
2016-11-28 21:22:10 -0500

    refactor(counter): Removes count up from repo and sets base values in counter component
    undefined

--- src/app/components/counter/counter.component.ts
+++ src/app/components/counter/counter.component.ts
@@ -7,29 +7,10 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CounterComponent implements OnInit {
   manuals = 15;
-  videos = 55;
-  prodYears = 41;
+  videos = 45;
+  productionYears = 41;
   brandOwners = 10;
 
   constructor() { }
-
-  ngOnInit() {
-    // const easingFn = function (t, b, c, d) {
-    //   const ts = (t /= d) * t;
-    //   const tc = ts * t;
-    //   return b + c * (tc + -3 * ts + 3 * t);
-    // };
-    // const options = {
-    //   useEasing : true,
-    //   easingFn: easingFn,
-    //   useGrouping : true,
-    //   separator : ',',
-    //   decimal : '.',
-    //   prefix : '',
-    //   suffix : ''
-    // };
-    // const demo = new CountUp('myTargetElement', 0, 3248, 0, 2.5, options);
-    // demo.start();
-  }
-
+  ngOnInit() {}
 }
