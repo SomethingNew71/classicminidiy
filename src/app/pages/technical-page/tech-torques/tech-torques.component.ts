de84421f8744f6b5ccec9ab819aed13aca612f2c
Cole Gentry <Peapod2007@gmail.com>
2016-12-07 22:05:00 -0500

    feat(App): Updates entire app with tons of restructure
    Adds data for technical section, adds plan of action for tech page, adds some new styling, moves
    
    toolbar out of the body of the page
    
    undefined

---
+++ src/app/pages/technical-page/tech-torques/tech-torques.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-tech-torques',
+  templateUrl: './tech-torques.component.html',
+  styleUrls: ['./tech-torques.component.scss']
+})
+export class TechTorquesComponent implements OnInit {
+  title = 'Torque Specs';
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
--- src/app/pages/technical-page/tech-welcome/tech-welcome.component.ts
+++ src/app/pages/technical-page/tech-welcome/tech-welcome.component.ts
@@ -6,7 +6,36 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./tech-welcome.component.scss']
 })
 export class TechWelcomeComponent implements OnInit {
-
+  dimensions = {
+    wheelbase: {
+      saloonIn: 80.2,
+      saloonMm: 2036,
+      estateIn: 84.2,
+      estateMm: 2138
+    },
+    length: {
+      saloonIn: 120.2,
+      saloonMm: 3054,
+      estateIn: 129.9,
+      estateMm: 3299,
+      hornetIn: 130,
+      hornetMm: 3300
+    },
+    width: {
+      saloonIn: 55,
+      saloonMm: 1397
+    },
+    height: {
+      saloonIn: 53,
+      saloonMm: 1346
+    },
+    Weight: {
+      saloonLb: 1360,
+      saloonkg: 617,
+      estateLb: 1512,
+      estatekg: 686
+    }
+  };
   constructor() { }
 
   ngOnInit() {
