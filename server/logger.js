9330a2ce37ef65d0087b797cb21f97b8a20ddeba
Cole Gentry <Peapod2007@gmail.com>
2017-02-22 21:06:18 -0500

    Gets the react boilerplate into a good place

--- server/logger.js
+++ server/logger.js
@@ -29,7 +29,7 @@ ${chalk.bold('Access URLs:')}${divider}
 Localhost: ${chalk.magenta(`http://${host}:${port}`)}
       LAN: ${chalk.magenta(`http://${ip.address()}:${port}`) +
 (tunnelStarted ? `\n    Proxy: ${chalk.magenta(tunnelStarted)}` : '')}${divider}
-${chalk.blue(`Press ${chalk.italic('CTRL-C')} to stop`)}
+${chalk.green(`Press ${chalk.italic('CTRL-C')} to stop`)}
     `);
   },
 };
