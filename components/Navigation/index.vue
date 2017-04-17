a794b4f29a83bc850895e9f769412efb0bff3534
Cole Gentry <Peapod2007@gmail.com>
2017-04-17 18:01:04 -0400

    build(Repo): Updates repo and builds more of the mini app

--- components/Navigation/index.vue
+++ components/Navigation/index.vue
@@ -7,19 +7,6 @@
       </nuxt-link>
     </div>
 
-    <div class="nav-center">
-      <a class="nav-item">
-        <span class="icon">
-          <i class="fa fa-github"></i>
-        </span>
-      </a>
-      <a class="nav-item">
-        <span class="icon">
-          <i class="fa fa-twitter"></i>
-        </span>
-      </a>
-    </div>
-
     <!-- This "nav-toggle" hamburger menu is only visible on mobile -->
     <!-- You need JavaScript to toggle the "is-active" class on "nav-menu" -->
     <span class="nav-toggle">
@@ -38,12 +25,6 @@
       <nuxt-link class="nav-item" to="/historical">Historical</nuxt-link>
       <nuxt-link class="nav-item" to="/contact">Contact</nuxt-link>
       <span class="nav-item">
-        <a class="button" >
-          <span class="icon">
-            <i class="fa fa-twitter"></i>
-          </span>
-          <span>Tweet</span>
-        </a>
         <a class="button is-primary">
           <span class="icon">
             <i class="fa fa-download"></i>
@@ -53,25 +34,6 @@
       </span>
     </div>
   </nav>
-  <!-- <nav class="main-nav">
-    <ul>
-      <li>
-        <nuxt-link to="/about">About</nuxt-link>
-      </li>
-      <li>
-        <nuxt-link to="/specs">Specs</nuxt-link>
-      </li>
-      <li>
-        <nuxt-link to="/manuals">Manuals</nuxt-link>
-      </li>
-      <li>
-        <nuxt-link to="/historical">Historical</nuxt-link>
-      </li>
-      <li>
-        <nuxt-link to="/contact">Contact</nuxt-link>
-      </li>
-    </ul>
-  </nav> -->
 </template>
 
 <style lang="scss" src="./navigation.scss"></style>
