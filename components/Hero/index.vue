1b5ed786fbfc7591678dfa029b162108c787214f
Cole Gentry <Peapod2007@gmail.com>
2017-04-24 21:17:49 -0400

    Updates package.json

--- components/Hero/index.vue
+++ components/Hero/index.vue
@@ -2,7 +2,7 @@
   <section class="hero is-dark is-medium is-bold is-full-width">
     <div class="hero-body">
       <div class="container">
-        <h1 class="title">The Number One Source of Classic Mini Information</h1>
+        <h1 class="title">The Best Source of Classic Mini Information</h1>
       </div>
     </div>
   </section>
