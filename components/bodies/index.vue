79e6f330914ddab39d9db279a71e9f3512a07b22
Cole Gentry <Peapod2007@gmail.com>
2017-06-04 18:42:41 -0400

    Adds two new components with toggle functionality in specs

---
+++ components/bodies/index.vue
@@ -0,0 +1,278 @@
+<template>
+  <div class="column bodies">
+    <h2 class="title">Bodies</h2>
+    <div class="tabs">
+      <ul>
+        <li v-bind:class="{ 'is-active': saloonActive }" class="saloon">
+          <a v-on:click="saloonToggle" >
+            <span class="icon is-small"><i class="fa fa-black-tie"></i></span>
+            <span>Saloon</span>
+          </a>
+        </li>
+        <li v-bind:class="{ 'is-active': estateActive }" class="estate">
+          <a v-on:click="estateToggle">
+            <span class="icon is-small"><i class="fa fa-users"></i></span>
+            <span>Estate</span>
+          </a>
+        </li>
+        <li v-bind:class="{ 'is-active': elfActive }" class="elf">
+          <a v-on:click="elfToggle">
+            <span class="icon is-small"><i class="fa fa-question"></i></span>
+            <span>Elf</span>
+          </a>
+        </li>
+        <li v-bind:class="{ 'is-active': mokeActive }" class="moke">
+          <a v-on:click="mokeToggle">
+            <span class="icon is-small"><i class="fa fa-hand-peace-o"></i></span>
+            <span>Moke</span>
+          </a>
+        </li>
+      </ul>
+    </div>
+    <!-- SALOON *************************************************************-->
+    <div v-bind:class="{ 'is-active': saloonActive }" class="columns saloon">
+      <div class="column is-4">
+        <div class="card">
+          <div class="card-content">
+            <h3 class="title">
+              Classic Mini Saloon
+            </h3>
+            <p class="subtitle">
+              The most iconic of the Classic mini body styles, the Saloon is what most people attribute to the Classic Mini Cooper.
+            </p>
+          </div>
+        </div>
+        <div class="columns">
+          <div class="column is-half aside-value">
+            <h3 class="title"></h3>
+            <p>Weight</p>
+            <hr>
+          </div>
+          <div class="column is-half aside-value">
+            <h3 class="title">in</h3>
+            <p>Wheelbase</p>
+            <hr>
+          </div>
+        </div>
+      </div>
+      <div class="column">
+        Second column
+      </div>
+    </div>
+    <!-- ESTATE *************************************************************-->
+    <div v-bind:class="{ 'is-active': estateActive }" class="columns estate">
+      <div class="column is-4">
+        <div class="card">
+          <div class="card-content">
+            <h3 class="title">
+              Classic Mini Saloon
+            </h3>
+            <p class="subtitle">
+              The most iconic of the Classic mini body styles, the Saloon is what most people attribute to the Classic Mini Cooper.
+            </p>
+          </div>
+        </div>
+        <div class="columns">
+          <div class="column is-half aside-value">
+            <h3 class="title"></h3>
+            <p>Weight</p>
+            <hr>
+          </div>
+          <div class="column is-half aside-value">
+            <h3 class="title">in</h3>
+            <p>Wheelbase</p>
+            <hr>
+          </div>
+        </div>
+      </div>
+      <div class="column">
+        Second column
+      </div>
+    </div>
+    <!-- ELF ****************************************************************-->
+    <div v-bind:class="{ 'is-active': elfActive }" class="columns elf">
+      <div class="column is-4">
+        <div class="card">
+          <div class="card-content">
+            <h3 class="title">
+              Classic Mini Saloon
+            </h3>
+            <p class="subtitle">
+              The most iconic of the Classic mini body styles, the Saloon is what most people attribute to the Classic Mini Cooper.
+            </p>
+          </div>
+        </div>
+        <div class="columns">
+          <div class="column is-half aside-value">
+            <h3 class="title"></h3>
+            <p>Weight</p>
+            <hr>
+          </div>
+          <div class="column is-half aside-value">
+            <h3 class="title">in</h3>
+            <p>Wheelbase</p>
+            <hr>
+          </div>
+        </div>
+      </div>
+      <div class="column">
+        Second column
+      </div>
+    </div>
+    <!-- MOKE ***************************************************************-->
+    <div v-bind:class="{ 'is-active': mokeActive }" class="columns moke">
+      <div class="column is-4">
+        <div class="card">
+          <div class="card-content">
+            <h3 class="title">
+              Classic Mini Saloon
+            </h3>
+            <p class="subtitle">
+              The most iconic of the Classic mini body styles, the Saloon is what most people attribute to the Classic Mini Cooper.
+            </p>
+          </div>
+        </div>
+        <div class="columns">
+          <div class="column is-half aside-value">
+            <h3 class="title"></h3>
+            <p>Weight</p>
+            <hr>
+          </div>
+          <div class="column is-half aside-value">
+            <h3 class="title">in</h3>
+            <p>Wheelbase</p>
+            <hr>
+          </div>
+        </div>
+      </div>
+      <div class="column">
+        Second column
+      </div>
+    </div>
+  </div>
+</template>
+
+<style lang="scss" src="./bodies.scss"></style>
+
+<script>
+import specs from '~pages/specs';
+
+export default {
+  components: {
+    specs
+  },
+  data: function () {
+    return {
+      saloonActive: true,
+      estateActive: false,
+      elfActive: false,
+      mokeActive: false,
+      'unit': {
+        'weight': {
+          'metric': 'kg',
+          'imperial': 'lbs'
+        },
+        'length': {
+          'metric': 'mm',
+          'imperial': 'in'
+        },
+        'power': {
+          'metric': 'kw',
+          'imperial': 'hp'
+        },
+        'torque': {
+          'metric': 'nm',
+          'imperial': 'lb/ft'
+        }
+      },
+      'body': {
+        'wheelbase': {
+          'metric': {
+            'saloon': 2036,
+            'estate': 2138
+          },
+          'imperial': {
+            'saloon': 80.2,
+            'estate': 84.2
+          }
+        },
+        'length': {
+          'metric': {
+            'saloon': 3054,
+            'estate': 3299,
+            'hornet': 3300
+          },
+          'imperial': {
+            'saloon': 120.2,
+            'estate': 129.9,
+            'hornet': 130
+          }
+        },
+        'width': {
+          'metric': {
+            'saloon': 1397,
+            'estate': 1397,
+            'hornet': 1397
+          },
+          'imperial': {
+            'saloon': 55,
+            'estate': 55,
+            'hornet': 55
+          }
+        },
+        'height': {
+          'metric': {
+            'saloon': 1346,
+            'estate': 1346,
+            'hornet': 1346
+          },
+          'imperial': {
+            'saloon': 53,
+            'estate': 53,
+            'hornet': 53
+          }
+        },
+        'weight': {
+          'metric': {
+            'saloon': 617,
+            'estate': 686,
+            'hornet': 617
+          },
+          'imperial': {
+            'saloon': 1360,
+            'estate': 1512,
+            'hornet': 1360
+          }
+        }
+      }
+    };
+  },
+  methods: {
+    saloonToggle: function () {
+      console.log(specs.isMetric);
+      this.saloonActive = true;
+      this.estateActive = false;
+      this.elfActive = false;
+      this.mokeActive = false;
+    },
+    estateToggle: function () {
+      this.saloonActive = false;
+      this.estateActive = true;
+      this.elfActive = false;
+      this.mokeActive = false;
+    },
+    elfToggle: function () {
+      this.saloonActive = false;
+      this.estateActive = false;
+      this.elfActive = true;
+      this.mokeActive = false;
+    },
+    mokeToggle: function () {
+      this.saloonActive = false;
+      this.estateActive = false;
+      this.elfActive = false;
+      this.mokeActive = true;
+    }
+  }
+};
+</script>
--- pages/specs/index.vue
+++ pages/specs/index.vue
@@ -2,12 +2,12 @@
   <section>
     <div class="container specs">
       <div class="columns header">
-        <div class="column is-10">
+        <div class="column is-9">
           <h1 class="title">
             Specifications
           </h1>
         </div>
-        <div class="column">
+        <!-- <div class="column">
           <div class="tabs is-toggle">
             <ul>
               <li class="is-active">
@@ -24,94 +24,13 @@
               </li>
             </ul>
           </div>
-        </div>
-      </div>
-      <h2 class="title">Body</h2>
-      <div class="tabs">
-        <ul>
-          <li class="is-active">
-            <a>
-              <span class="icon is-small"><i class="fa fa-black-tie"></i></span>
-              <span>Saloon</span>
-            </a>
-          </li>
-          <li>
-            <a>
-              <span class="icon is-small"><i class="fa fa-users"></i></span>
-              <span>Estate</span>
-            </a>
-          </li>
-          <li>
-            <a>
-              <span class="icon is-small"><i class="fa fa-question"></i></span>
-              <span>Elf</span>
-            </a>
-          </li>
-          <li>
-            <a>
-              <span class="icon is-small"><i class="fa fa-hand-peace-o"></i></span>
-              <span>Moke</span>
-            </a>
-          </li>
-        </ul>
+        </div> -->
       </div>
       <div class="columns">
-        <div class="column is-4">
-          <div class="card">
-            <div class="card-content">
-              <h3 class="title">
-                Classic Mini Saloon
-              </h3>
-              <p class="subtitle">
-                The most iconic of the Classic mini body styles, the Saloon is what most people attribute to the Classic Mini Cooper.
-              </p>
-            </div>
-          </div>
-          <div class="columns">
-            <div class="column is-half aside-value">
-              <h3 class="title">{{ body.weight.saloonLb}}lbs</h3>
-              <p>Weight</p>
-              <hr>
-            </div>
-            <div class="column is-half aside-value">
-              <h3 class="title">{{ body.wheelbase.saloonIn}}in</h3>
-              <p>Wheelbase</p>
-              <hr>
-            </div>
-          </div>
-        </div>
-        <div class="column">
-          Second column
-        </div>
+        <bodies/>
       </div>
-      <h2 class="title">Engine</h2>
-      <div class="tabs">
-        <ul>
-          <li class="is-active">
-            <a>
-              <span class="icon is-small"><i class="fa fa-black-tie"></i></span>
-              <span>Saloon</span>
-            </a>
-          </li>
-          <li>
-            <a>
-              <span class="icon is-small"><i class="fa fa-users"></i></span>
-              <span>Estate</span>
-            </a>
-          </li>
-          <li>
-            <a>
-              <span class="icon is-small"><i class="fa fa-question"></i></span>
-              <span>Elf</span>
-            </a>
-          </li>
-          <li>
-            <a>
-              <span class="icon is-small"><i class="fa fa-hand-peace-o"></i></span>
-              <span>Moke</span>
-            </a>
-          </li>
-        </ul>
+      <div class="columns">
+        <engines/>
       </div>
     </div>
   </section>
@@ -119,189 +38,14 @@
 
 <style lang="scss" src="./specs.scss"></style>
 <script>
-export default {
-  data: function () {
-    return {
-      'unit': {
-        'weight': {
-          'metric': 'kg',
-          'imperial': 'lbs'
-        },
-        'length': {
-          'metric': 'mm',
-          'imperial': 'in'
-        },
-        'power': {
-          'metric': 'kw',
-          'imperial': 'hp'
-        },
-        'torque': {
-          'metric': 'nm',
-          'imperial': 'lb/ft'
-        }
-      },
-      'body': {
-        'wheelbase': {
-          'metric': {
-            'saloon': 2036,
-            'estate': 2138
-          },
-          'imperial': {
-            'saloon': 80.2,
-            'estate': 84.2
-          }
-        },
-        'length': {
-          'metric': {
-            'saloon': 3054,
-            'estate': 3299,
-            'hornet': 3300
-          },
-          'imperial': {
-            'saloon': 120.2,
-            'estate': 129.9,
-            'hornet': 130
-          }
-        },
-        'width': {
-          'metric': {
-            'saloon': 1397,
-            'estate': 1397,
-            'hornet': 1397
-          },
-          'imperial': {
-            'saloon': 55,
-            'estate': 55,
-            'hornet': 55
-          }
-        },
-        'height': {
-          'metric': {
-            'saloon': 1346,
-            'estate': 1346,
-            'hornet': 1346
-          },
-          'imperial': {
-            'saloon': 53,
-            'estate': 53,
-            'hornet': 53
-          }
-        },
-        'weight': {
-          'metric': {
-            'saloon': 617,
-            'estate': 686,
-            'hornet': 617
-          },
-          'imperial': {
-            'saloon': 1360,
-            'estate': 1512,
-            'hornet': 1360
-          }
-        }
-      },
-      'engines': {
-        'engine850a': {
-          'prodYears': '1959 - 1980',
-          'displacement': 850,
-          'power': {
-            'metric': 25,
-            'imperial': 34
-          },
-          'torque': {
-            'metric': 60,
-            'imperial': 44
-          }
-        },
-        'engine998a': {
-          'prodYears': '1959 - 1980',
-          'displacement': 998,
-          'power': {
-            'metric': 31,
-            'imperial': 41
-          },
-          'torque': {
-            'metric': 71,
-            'imperial': 52
-          }
-        },
-        'engine1071a': {
-          'prodYears': '1963 - 1964',
-          'displacement': 1071,
-          'power': {
-            'metric': 52,
-            'imperial': 70
-          },
-          'torque': {
-            'metric': 84,
-            'imperial': 62
-          }
-        },
-        'engine1098a': {
-          'prodYears': '1969 - 1980',
-          'displacement': 1098,
-          'power': {
-            'metric': 34,
-            'imperial': 45
-          },
-          'torque': {
-            'metric': 76,
-            'imperial': 56
-          }
-        },
-        'engine1275a': {
-          'prodYears': '1964 - 1980',
-          'displacement': 1275,
-          'power': {
-            'metric': 57,
-            'imperial': 76
-          },
-          'torque': {
-            'metric': 107,
-            'imperial': 79
-          }
-        },
-        'engine998plus': {
-          'prodYears': '1980 - 1992',
-          'displacement': 998,
-          'power': {
-            'metric': 33,
-            'imperial': 44
-          },
-          'torque': {
-            'metric': 71,
-            'imperial': 52
-          }
-        },
-        'engine1275plus': {
-          'prodYears': '1980 - 1996',
-          'displacement': 1275,
-          'power': {
-            'metric': 58,
-            'imperial': 78
-          },
-          'torque': {
-            'metric': 106,
-            'imperial': 78
-          }
-        },
-        'engine1275MPIplus': {
-          'prodYears': '1997 - 2000',
-          'displacement': 1275,
-          'power': {
-            'metric': 47,
-            'imperial': 63
-          },
-          'torque': {
-            'metric': 95,
-            'imperial': 70
-          }
-        }
-      }
-    };
-  },
-  methods: {
+import bodies from '~components/bodies';
+import engines from '~components/engines';
 
-  }
+export default {
+  components: {
+    bodies,
+    engines
+  },
+  methods: {}
 };
 </script>
