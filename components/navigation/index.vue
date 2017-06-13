0437007a3f37eada778cf23ba11b7ccef7f58f2a
Cole Gentry <Peapod2007@gmail.com>
2017-06-13 08:44:16 -0400

    lays out manual data in prep for books

--- components/navigation/index.vue
+++ components/navigation/index.vue
@@ -19,11 +19,29 @@
 
     <!-- This "nav-menu" is hidden on mobile -->
     <!-- Add the modifier "is-active" to display it on mobile -->
-    <div v-bind:class="{ 'is-active': isActive }" class="nav-right nav-menu">
-      <nuxt-link class="nav-item" to="/">Home</nuxt-link>
-      <nuxt-link class="nav-item" to="/specs">Specs</nuxt-link>
-      <nuxt-link class="nav-item" to="/manuals">Manuals</nuxt-link>
-      <nuxt-link class="nav-item" to="/historical">Historical</nuxt-link>
+    <div
+      v-bind:class="{ 'is-active': isActive }"
+      class="nav-right nav-menu">
+      <nuxt-link
+        class="nav-item"
+        to="/">
+        Home
+      </nuxt-link>
+      <nuxt-link
+        class="nav-item"
+        to="/specs">
+        Specs
+      </nuxt-link>
+      <nuxt-link
+        class="nav-item"
+        to="/manuals">
+        Manuals
+      </nuxt-link>
+      <nuxt-link
+        class="nav-item"
+        to="/historical">
+        Historical
+      </nuxt-link>
       <!-- <nuxt-link class="nav-item" href="">Contact</nuxt-link> -->
       <span class="nav-item">
         <a class="button is-primary" href="https://www.youtube.com/c/classicminidiy" target="_blank">
@@ -42,7 +60,9 @@
 <script>
 export default {
   data: function () {
-    return { isActive: false };
+    return {
+      isActive: false
+    };
   },
   methods: {
     toggleNav: function () {
