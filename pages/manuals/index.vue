efd117b333c5a637e848ab2e7464f00800d1d8c6
Cole Gentry <Peapod2007@gmail.com>
2017-06-14 22:05:10 -0400

    Implements conditional loading of affiliate links on manuals

--- pages/manuals/index.vue
+++ pages/manuals/index.vue
@@ -57,10 +57,10 @@
               </div>
             </div>
             <footer class="card-footer columns">
-              <div class="column">
+              <div class="column" v-if='book.urlAmazon'>
                 <a class="card-footer-item amazon" :href='book.urlAmazon'><span class="fa fa-amazon"></span>Amazon</a>
               </div>
-              <div class="column">
+              <div class="column" v-if='book.urlSeven'>
                 <a class="card-footer-item seven-ent"><span class="fa fa-car"></span>Seven Mini</a>
               </div>
             </footer>
@@ -84,10 +84,10 @@
               </div>
             </div>
             <footer class="card-footer columns">
-              <div class="column">
+              <div class="column"  v-if='book.urlAmazon'>
                 <a class="card-footer-item amazon" :href='book.urlAmazon'><span class="fa fa-amazon"></span>Amazon</a>
               </div>
-              <div class="column">
+              <div class="column" v-if='book.urlSeven'>
                 <a class="card-footer-item seven-ent"><span class="fa fa-car"></span>Seven Mini</a>
               </div>
             </footer>
@@ -109,6 +109,7 @@
             1: {
               'name': 'Haynes Mini - 1959 to 1969',
               'author': 'John Haynes',
+              'urlSeven': '',
               'urlAmazon': 'http://amzn.to/2s8NTEJ',
               'imgSource': '//ws-na.amazon-adsystem.com/widgets/q?_encoding=UTF8&ASIN=0856965278&Format=_SL250_&ID=AsinImage&MarketPlace=US&ServiceVersion=20070822&WS=1&tag=classicminidi-20',
               'imgFrame': '<a href="https://www.amazon.com/Haynes-Mini-Owners-Workshop-Manual/dp/0856965278/ref=as_li_ss_il?ie=UTF8&qid=1497403488&sr=8-4&keywords=Mini+(1959+-+1969)+haynes&linkCode=li3&tag=classicminidi-20&linkId=d4dedc0e81a46e63e0567c7190a93ad1" target="_blank"><img border="0" src="//ws-na.amazon-adsystem.com/widgets/q?_encoding=UTF8&ASIN=0856965278&Format=_SL250_&ID=AsinImage&MarketPlace=US&ServiceVersion=20070822&WS=1&tag=classicminidi-20" ></a><img src="https://ir-na.amazon-adsystem.com/e/ir?t=classicminidi-20&l=li3&o=1&a=0856965278" width="1" height="1" border="0" alt="" style="border:none !important; margin:0px !important;" />',
@@ -117,6 +118,7 @@
             2: {
               'name': 'Haynes Mini - 1969 to 2001',
               'author': 'John S Mead',
+              'urlSeven': '',
               'urlAmazon': 'http://amzn.to/2slJsZ8',
               'imgSource': '//ws-na.amazon-adsystem.com/widgets/q?_encoding=UTF8&ASIN=0857339656&Format=_SL250_&ID=AsinImage&MarketPlace=US&ServiceVersion=20070822&WS=1&tag=classicminidi-20',
               'imgFrame': '<a href="https://www.amazon.com/Haynes-Registration-Service-Repair-Manual/dp/0857339656/ref=as_li_ss_il?ie=UTF8&qid=1497396942&sr=8-3&keywords=Mini+Repair+manual&linkCode=li3&tag=classicminidi-20&linkId=b2121622386f905e03457864c07ce73b" target="_blank"><img border="0" src="//ws-na.amazon-adsystem.com/widgets/q?_encoding=UTF8&ASIN=0857339656&Format=_SL250_&ID=AsinImage&MarketPlace=US&ServiceVersion=20070822&WS=1&tag=classicminidi-20" ></a><img src="https://ir-na.amazon-adsystem.com/e/ir?t=classicminidi-20&l=li3&o=1&a=0857339656" width="1" height="1" border="0" alt="" style="border:none !important; margin:0px !important;" />" />',
@@ -125,6 +127,7 @@
             3: {
               'name': 'Mini Workshop Manual (Official Workshop Manuals)',
               'author': 'Brooklands Books Ltd',
+              'urlSeven': '',
               'urlAmazon': 'http://amzn.to/2rxfPQB',
               'imgSource': '//ws-na.amazon-adsystem.com/widgets/q?_encoding=UTF8&ASIN=1855201488&Format=_SL250_&ID=AsinImage&MarketPlace=US&ServiceVersion=20070822&WS=1&tag=classicminidi-20',
               'imgFrame': '<a href="https://www.amazon.com/Mini-Workshop-Manual-Official-Manuals/dp/1855201488/ref=as_li_ss_il?_encoding=UTF8&pd_rd_i=1855201488&pd_rd_r=THVAMSQ4PVRSSBE7STXP&pd_rd_w=zbyWB&pd_rd_wg=Idzj1&psc=1&refRID=THVAMSQ4PVRSSBE7STXP&linkCode=li3&tag=classicminidi-20&linkId=323598647a4604ae498f65df80eaa06d" target="_blank"><img border="0" src="//ws-na.amazon-adsystem.com/widgets/q?_encoding=UTF8&ASIN=1855201488&Format=_SL250_&ID=AsinImage&MarketPlace=US&ServiceVersion=20070822&WS=1&tag=classicminidi-20" ></a><img src="https://ir-na.amazon-adsystem.com/e/ir?t=classicminidi-20&l=li3&o=1&a=1855201488" width="1" height="1" border="0" alt="" style="border:none !important; margin:0px !important;" />',
@@ -133,6 +136,7 @@
             4: {
               'name': 'Mini: Purchase and Restoration Guide',
               'author': 'Lindsay Porter',
+              'urlSeven': '',
               'urlAmazon': 'http://amzn.to/2smk1Xs',
               'imgSource': '//ws-na.amazon-adsystem.com/widgets/q?_encoding=UTF8&ASIN=1859604404&Format=_SL250_&ID=AsinImage&MarketPlace=US&ServiceVersion=20070822&WS=1&tag=classicminidi-20',
               'imgFrame': '<a href="https://www.amazon.com/Mini-Purchase-Restoration-Lindsay-Porter/dp/1859604404/ref=as_li_ss_il?ie=UTF8&qid=1497404189&sr=8-1&keywords=classic+mini+haynes&linkCode=li3&tag=classicminidi-20&linkId=6c84288fe26c533e4388c10980311786" target="_blank"><img border="0" src="//ws-na.amazon-adsystem.com/widgets/q?_encoding=UTF8&ASIN=1859604404&Format=_SL250_&ID=AsinImage&MarketPlace=US&ServiceVersion=20070822&WS=1&tag=classicminidi-20" ></a><img src="https://ir-na.amazon-adsystem.com/e/ir?t=classicminidi-20&l=li3&o=1&a=1859604404" width="1" height="1" border="0" alt="" style="border:none !important; margin:0px !important;" />',
@@ -141,6 +145,7 @@
             5: {
               'name': 'Mini Renovation Manual - 1986 to 2000',
               'author': 'Paul Hardiman',
+              'urlSeven': '',
               'urlAmazon': 'http://amzn.to/2slQ6P1',
               'imgSource': '//ws-na.amazon-adsystem.com/widgets/q?_encoding=UTF8&ASIN=1844254240&Format=_SL250_&ID=AsinImage&MarketPlace=US&ServiceVersion=20070822&WS=1&tag=classicminidi-20',
               'imgFrame': '<a href="https://www.amazon.com/Mini-Renovation-Manual-1986-2000-Manuals/dp/1844254240/ref=as_li_ss_il?ie=UTF8&qid=1497404189&sr=8-3&keywords=classic+mini+haynes&linkCode=li3&tag=classicminidi-20&linkId=3b3d7ff756af1373ed670b266e5bae5e" target="_blank"><img border="0" src="//ws-na.amazon-adsystem.com/widgets/q?_encoding=UTF8&ASIN=1844254240&Format=_SL250_&ID=AsinImage&MarketPlace=US&ServiceVersion=20070822&WS=1&tag=classicminidi-20" ></a><img src="https://ir-na.amazon-adsystem.com/e/ir?t=classicminidi-20&l=li3&o=1&a=1844254240" width="1" height="1" border="0" alt="" style="border:none !important; margin:0px !important;" />',
@@ -149,6 +154,7 @@
             6: {
               'name': 'Mini Performance Manual',
               'author': 'Tim Mundy',
+              'urlSeven': '',
               'urlAmazon': 'http://amzn.to/2rp5Hho',
               'imgSource': '//ws-na.amazon-adsystem.com/widgets/q?_encoding=UTF8&ASIN=1859608809&Format=_SL250_&ID=AsinImage&MarketPlace=US&ServiceVersion=20070822&WS=1&tag=classicminidi-20',
               'imgFrame': '<a href="https://www.amazon.com/Mini-Performance-Manual-Haynes/dp/1859608809/ref=as_li_ss_il?ie=UTF8&qid=1497404189&sr=8-4&keywords=classic+mini+haynes&linkCode=li3&tag=classicminidi-20&linkId=264ff7a930a5319d1d8738b85ca4b3ef" target="_blank"><img border="0" src="//ws-na.amazon-adsystem.com/widgets/q?_encoding=UTF8&ASIN=1859608809&Format=_SL250_&ID=AsinImage&MarketPlace=US&ServiceVersion=20070822&WS=1&tag=classicminidi-20" ></a><img src="https://ir-na.amazon-adsystem.com/e/ir?t=classicminidi-20&l=li3&o=1&a=1859608809" width="1" height="1" border="0" alt="" style="border:none !important; margin:0px !important;" />',
@@ -157,6 +163,7 @@
             7: {
               'name': 'Mini: The Essential Buyer\'s Guide',
               'author': 'Mark Paxton',
+              'urlSeven': '',
               'urlAmazon': 'http://amzn.to/2rimn6n',
               'imgSource': '//ws-na.amazon-adsystem.com/widgets/q?_encoding=UTF8&ASIN=1845842049&Format=_SL250_&ID=AsinImage&MarketPlace=US&ServiceVersion=20070822&WS=1&tag=classicminidi-20',
               'imgFrame': '<a href="https://www.amazon.com/Mini-Essential-Buyers-Mark-Paxton/dp/1845842049/ref=as_li_ss_il?_encoding=UTF8&pd_rd_i=1845842049&pd_rd_r=THVAMSQ4PVRSSBE7STXP&pd_rd_w=U0WnN&pd_rd_wg=Idzj1&psc=1&refRID=THVAMSQ4PVRSSBE7STXP&linkCode=li3&tag=classicminidi-20&linkId=ef34c71b59ab81868c4f22b6631d25d2" target="_blank"><img border="0" src="//ws-na.amazon-adsystem.com/widgets/q?_encoding=UTF8&ASIN=1845842049&Format=_SL250_&ID=AsinImage&MarketPlace=US&ServiceVersion=20070822&WS=1&tag=classicminidi-20" ></a><img src="https://ir-na.amazon-adsystem.com/e/ir?t=classicminidi-20&l=li3&o=1&a=1845842049" width="1" height="1" border="0" alt="" style="border:none !important; margin:0px !important;" />',
@@ -167,6 +174,7 @@
             1: {
               'name': 'Anatomy of the Classic Mini',
               'author': 'Mark Huthert',
+              'urlSeven': '',
               'urlAmazon': 'http://amzn.to/2smpg9p',
               'imgSource': '//ws-na.amazon-adsystem.com/widgets/q?_encoding=UTF8&ASIN=1845842235&Format=_SL250_&ID=AsinImage&MarketPlace=US&ServiceVersion=20070822&WS=1&tag=classicminidi-20',
               'imgFrame': '<a href="https://www.amazon.com/Anatomy-Classic-Mini-definitive-interchangeability/dp/1845842235/ref=as_li_ss_il?ie=UTF8&qid=1497404041&sr=8-1&keywords=classic+mini+books&linkCode=li3&tag=classicminidi-20&linkId=35f89a9bf08117dd6821420954873b44" target="_blank"><img border="0" src="//ws-na.amazon-adsystem.com/widgets/q?_encoding=UTF8&ASIN=1845842235&Format=_SL250_&ID=AsinImage&MarketPlace=US&ServiceVersion=20070822&WS=1&tag=classicminidi-20" ></a><img src="https://ir-na.amazon-adsystem.com/e/ir?t=classicminidi-20&l=li3&o=1&a=1845842235" width="1" height="1" border="0" alt="" style="border:none !important; margin:0px !important;" />',
@@ -175,6 +183,7 @@
             2: {
               'name': 'Tuning the A-Series Engine',
               'author': 'David Vizard',
+              'urlSeven': '',
               'urlAmazon': 'http://amzn.to/2t3cRoR',
               'imgSource': '//ws-na.amazon-adsystem.com/widgets/q?_encoding=UTF8&ASIN=1859606202&Format=_SL250_&ID=AsinImage&MarketPlace=US&ServiceVersion=20070822&WS=1&tag=classicminidi-20',
               'imgFrame': '<a href="https://www.amazon.com/Tuning-Engine-Definitive-Performance-Economy/dp/1859606202/ref=as_li_ss_il?_encoding=UTF8&pd_rd_i=1859606202&pd_rd_r=THVAMSQ4PVRSSBE7STXP&pd_rd_w=U0WnN&pd_rd_wg=Idzj1&psc=1&refRID=THVAMSQ4PVRSSBE7STXP&linkCode=li3&tag=classicminidi-20&linkId=1cfae556a49124602a87095d62b176f1" target="_blank"><img border="0" src="//ws-na.amazon-adsystem.com/widgets/q?_encoding=UTF8&ASIN=1859606202&Format=_SL250_&ID=AsinImage&MarketPlace=US&ServiceVersion=20070822&WS=1&tag=classicminidi-20" ></a><img src="https://ir-na.amazon-adsystem.com/e/ir?t=classicminidi-20&l=li3&o=1&a=1859606202" width="1" height="1" border="0" alt="" style="border:none !important; margin:0px !important;" />',
@@ -183,6 +192,7 @@
             3: {
               'name': 'SU Carburetters Tuning Tips And Techniques',
               'author': 'R.M. Clarke',
+              'urlSeven': '',
               'urlAmazon': 'http://amzn.to/2to2D1D',
               'imgSource': '//ws-na.amazon-adsystem.com/widgets/q?_encoding=UTF8&ASIN=1855202557&Format=_SL250_&ID=AsinImage&MarketPlace=US&ServiceVersion=20070822&WS=1&tag=classicminidi-20',
               'imgFrame': '<a href="https://www.amazon.com/SU-Carburetters-Tuning-Tips-Techniques/dp/1855202557/ref=as_li_ss_il?_encoding=UTF8&pd_rd_i=1855202557&pd_rd_r=ADRNVNTZ6VVVCTW03KZN&pd_rd_w=my5cl&pd_rd_wg=J5SNd&psc=1&refRID=ADRNVNTZ6VVVCTW03KZN&linkCode=li3&tag=classicminidi-20&linkId=57ccd821731bd290ed006d2d8e124aee" target="_blank"><img border="0" src="//ws-na.amazon-adsystem.com/widgets/q?_encoding=UTF8&ASIN=1855202557&Format=_SL250_&ID=AsinImage&MarketPlace=US&ServiceVersion=20070822&WS=1&tag=classicminidi-20" ></a><img src="https://ir-na.amazon-adsystem.com/e/ir?t=classicminidi-20&l=li3&o=1&a=1855202557" width="1" height="1" border="0" alt="" style="border:none !important; margin:0px !important;" />',
@@ -191,6 +201,7 @@
             4: {
               'name': 'The Ultimate Mini Restoration Manual',
               'author': 'Iain Ayre',
+              'urlSeven': '',
               'urlAmazon': 'http://amzn.to/2sqcmqV',
               'imgSource': '//ws-na.amazon-adsystem.com/widgets/q?_encoding=UTF8&ASIN=1845841166&Format=_SL250_&ID=AsinImage&MarketPlace=US&ServiceVersion=20070822&WS=1&tag=classicminidi-20',
               'imgFrame': '<a href="https://www.amazon.com/Ultimate-Mini-Restoration-Manual-Overpower/dp/1845841166/ref=as_li_ss_il?_encoding=UTF8&pd_rd_i=1845841166&pd_rd_r=ADRNVNTZ6VVVCTW03KZN&pd_rd_w=my5cl&pd_rd_wg=J5SNd&psc=1&refRID=ADRNVNTZ6VVVCTW03KZN&linkCode=li3&tag=classicminidi-20&linkId=ce31d3118c5dbf6c4201ec39ea42650f" target="_blank"><img border="0" src="//ws-na.amazon-adsystem.com/widgets/q?_encoding=UTF8&ASIN=1845841166&Format=_SL250_&ID=AsinImage&MarketPlace=US&ServiceVersion=20070822&WS=1&tag=classicminidi-20" ></a><img src="https://ir-na.amazon-adsystem.com/e/ir?t=classicminidi-20&l=li3&o=1&a=1845841166" width="1" height="1" border="0" alt="" style="border:none !important; margin:0px !important;" />',
