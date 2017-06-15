eba8321bc6759a0a8535a02bdf4e4b66abec9406
Cole Gentry <Peapod2007@gmail.com>
2017-06-14 22:17:40 -0400

    Tweaks to the history page adding a banner

--- pages/historical/index.vue
+++ pages/historical/index.vue
@@ -1,7 +1,18 @@
 <template>
-  <section>
-    <div class="historical">
+  <section class="historical">
+    <div>
       <div class="loader">
+      </div>
+      <div class="hero is-dark is-medium is-bold is-full-width">
+        <div class="hero-body">
+          <div class="container">
+            <div class="columns">
+              <div class="column">
+                <h1 class="title">Technical Literature and Tools</h1>
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
       <div class="iframe-container">
         <iframe src='https://cdn.knightlab.com/libs/timeline3/latest/embed/index.html?source=1UBqFp7aZ2qgC1_dlFTXOavaiMN8tSNxS-IPnYgU8e_g&font=PT&lang=en&initial_zoom=2&height=600' width='100%' height='600' webkitallowfullscreen mozallowfullscreen allowfullscreen frameborder='0'></iframe>
@@ -10,5 +21,10 @@
   </section>
 </template>
 
-<script src="./historical.js"></script>
+<script>
+  export default {
+    components: {
+    }
+  };
+</script>
 <style lang="scss" src="./historical.scss"></style>
