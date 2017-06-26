581d22e0e83ba99aaab45e0ac43299b9b7c175a1
Cole Gentry <Peapod2007@gmail.com>
2017-06-25 22:01:11 -0400

    Many changes working the app into the new framework

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,24 +1,11 @@
 <template>
   <v-app dark>
     <navigation/>
-    <v-toolbar fixed light>
-      <v-toolbar-side-icon light @click.native.stop="drawer = !drawer"></v-toolbar-side-icon>
-      <v-toolbar-title>Toolbar</v-toolbar-title>
-    </v-toolbar>
     <main>
-      <v-container fluid>
-        <nuxt/>
-      </v-container>
+      <nuxt/>
     </main>
-    <template>
-      <v-footer class="pa-3">
-        <v-spacer></v-spacer>
-        <div>Classic Mini DIY © - {{ new Date().getFullYear() }}</div>
-      </v-footer>
-    </template>
+    <pagefoot/> 
   </v-app>
-<!--
-    <pagefoot/> -->
 </template>
 
 <script>
