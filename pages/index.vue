7fc933f03952b2aa509831d596482651ac240581
Cole Gentry <Peapod2007@gmail.com>
2017-06-26 21:27:23 -0400

    MAjor layout fixing on homepage

--- pages/index.vue
+++ pages/index.vue
@@ -9,29 +9,42 @@
       </v-flex>
     </v-layout>
     <v-container fluid>
-      <v-layout row wrap class="mission">
-        <v-flex xs12 md4>
+      <v-layout row wrap class="mission" pt-4>
+        <v-flex xs12 md4 class="mission-header">
           <h2>The Mission</h2>
           <hr>
         </v-flex>
         <v-flex xs12 md8 class="mission-list">
-          <v-layout wrap>
-            <v-flex xs12>
-              <h3><img src="/supplementary/youtube-icon-small.png"> Create DIY Videos</h3>
+          <v-layout row>
+            <v-flex xs1>
+              <img class="youtube" src="/supplementary/youtube-icon-small.png" alt="">
+            </v-flex>
+            <v-flex xs11>
+              <h3>Create DIY Videos</h3>
               <br>
               <p>
                 Classic Mini DIY first and foremost is about creating DIY videos for new and inexperiences Classic Mini owners to use and reference when fixing their own cars.
               </p>
             </v-flex>
-            <v-flex xs12>
-              <h3><img src="/pages/home/books.png"> Consolidate Mini Knowledge</h3>
+          </v-layout>
+          <v-layout row>
+            <v-flex xs1>
+              <img class="books" src="/pages/home/books.png" alt="">
+            </v-flex>
+            <v-flex xs11>
+              <h3>Consolidate Mini Knowledge</h3>
               <br>
               <p>
                 As the channel expanded we started to realize that while there are many good sources of mini information out there, there is no single repository of technical and helpful knowledge for new mini owners. So with countless hours of work we have built just that.
               </p>
             </v-flex>
-            <v-flex xs12>
-              <h3><img src="/pages/home/grow.png"> Keep the Mini World Growing</h3>
+          </v-layout>
+          <v-layout row>
+            <v-flex xs1>
+              <img class="grow" src="/pages/home/grow.png" alt="">
+            </v-flex>
+            <v-flex xs11>
+              <h3>Keep the Mini World Growing</h3>
               <br>
               <p>
                 Our main mission is to keep the Classic minis out there on the road for many years to come and for countless others to find joy and excitement from these little cars built so many years ago.
@@ -41,7 +54,7 @@
         </v-flex>
       </v-layout>
       <v-layout row wrap class="latest-video">
-        <v-flex xs12 md4>
+        <v-flex xs12 md4 class="latest-video-header">
           <h2>Latest Videos</h2>
           <hr>
         </v-flex>
@@ -60,6 +73,30 @@
 // import footer from '~components/footer';
 
 export default {
+  data () {
+    return {
+      items: [
+        // { header: '' },
+        {
+          avatar: '/supplementary/youtube-icon-small.png',
+          title: 'Brunch this weekend?',
+          subtitle: '<h3 class="books">Consolidate Mini Knowledge</h3><p>As the channel expanded we started to realize that while there are many good sources of mini information out there, there is no single repository of technical and helpful knowledge for new mini owners. So with countless hours of work we have built just that.</p>'
+        },
+        { divider: true, inset: true },
+        {
+          avatar: '/pages/home/books.png',
+          title: 'Summer BBQ <span class="grey--text text--lighten-1">4</span>',
+          subtitle: "<span class='grey--text text--darken-2'>to Alex, Scott, Jennifer</span> — Wish I could come, but I'm out of town this weekend."
+        },
+        { divider: true, inset: true },
+        {
+          avatar: '/pages/home/grow.png',
+          title: 'Oui oui',
+          subtitle: "<span class='grey--text text--darken-2'>Sandra Adams</span> — Do you have Paris recommendations? Have you ever been?"
+        }
+      ]
+    };
+  },
   components: {
     // footer
   }
