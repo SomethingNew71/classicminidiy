f6e9229f6119f43c5ebc7b4c414991ea3d4d6f9d
Cole Gentry <Peapod2007@gmail.com>
2017-06-29 16:25:21 -0400

    :art: Ensures mobile styles look correct

--- pages/index.vue
+++ pages/index.vue
@@ -10,7 +10,7 @@
       </v-flex>
     </v-layout>
     <v-container fluid>
-      <v-layout row wrap class="mission pt-5 pb-5">
+      <v-layout row wrap class="mission pt-5 pb-4">
         <v-flex xs12 class="mission-header text-xs-center">
           <v-icon medium class="pb-2 primary--text" fa>certificate</v-icon>
           <p class="subheading grey--text lighten-2 ma-0">Keeping the classics alive</p>
@@ -18,7 +18,7 @@
         </v-flex>
       </v-layout>
       <v-layout row wrap class="mb-4 pt-3 text-xs-center mission-list">
-        <v-flex xs12 md4>
+        <v-flex xs12 md4 class="mb-4">
           <v-btn floating medium class="white mb-4">
             <v-icon fa class="red--text">youtube-play</v-icon>
           </v-btn>
@@ -28,7 +28,7 @@
             <v-btn class="red" light>Visit us on Youtube</v-btn>
           </a>
         </v-flex>
-        <v-flex xs12 md4 class="center">
+        <v-flex xs12 md4 class="center mb-4">
           <v-btn floating medium class="white mb-4">
             <v-icon fa class="primary--text">book</v-icon>
           </v-btn>
@@ -40,7 +40,7 @@
           <v-btn primary light router :to="history">History</v-btn>
           <v-btn primary light router :to="manuals">Manuals</v-btn> -->
         </v-flex>
-        <v-flex xs12 md4>
+        <v-flex xs12 md4 class="mb-4">
           <v-btn floating medium class="white mb-4">
             <v-icon fa class="green--text">tree</v-icon>
           </v-btn>
@@ -71,7 +71,7 @@
             <p class="subheading grey--text lighten-2 ma-0">All the wonderful people who help support this channel</p>
             <h2 class="black--text darken-3">Our Sponsors</h2>
           </v-flex>
-          <v-flex xs12 offset-md1 md5 class="first">
+          <v-flex xs12 offset-md1 md5 class="first mb-4 pb-4">
             <img src="/pages/home/seven-logo.png" alt="">
             <h3 class="headline pt-3">Seven Mini Parts</h3>
             <p>From almost the point of this channels creation Seven Mini Parts has been a brand and company partner. They took a chance on a small youtube channel that is now growing past my wildest dreams. If you are in the market for Mini parts please check them out!</p>
