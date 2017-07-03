2c738403dd0a36aa4ea1a1baf1450bc42524ed5a
Cole Gentry <Peapod2007@gmail.com>
2017-07-03 19:38:16 -0400

    :art: Begins to layout electrical diagrams table

---
+++ components/techdocs/index.vue
@@ -0,0 +1,19 @@
+<template>
+  <v-layout wrap>
+    <v-flex xs12>
+      <h2 class="headline pl-5 pt-4">Free Technical Documents</h2>
+    </v-flex>
+  </v-layout>
+</template>
+
+<style lang="scss" src="./techdocs.scss"></style>
+
+<script>
+export default {
+  data: function () {
+    return {
+      value: true
+    };
+  }
+};
+</script>
