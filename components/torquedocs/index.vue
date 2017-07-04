b743a8ceba9c48b7371abfa1672b60702bc978b4
Cole Gentry <Peapod2007@gmail.com>
2017-07-04 11:45:10 -0400

    :memo: Cites torque specs

--- components/torquedocs/index.vue
+++ components/torquedocs/index.vue
@@ -1,7 +1,8 @@
 <template>
   <v-layout wrap>
     <v-flex xs12>
-      <h2 class="headline pl-5 pt-4">Common Torque Settings</h2>
+      <h2 class="headline pl-5 pt-4 mb-0">Common Torque Settings</h2>
+      <aside class="title pl-5 pt-0 pb-0 mb-0 subheader">Torque Specs taken from Mini Haynes Manuals</aside>
     </v-flex>
     <v-flex xs12 offset-sm1 sm10 class="mt-4">
       <v-card>
