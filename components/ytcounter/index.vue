e2b9c8bfb44502a77bd36ae3b7053cf9f92d7e82
Cole Gentry <Peapod2007@gmail.com>
2017-07-15 19:47:31 -0400

    :art: :bug: Exchanges vue-resource for axios due to support

--- components/ytcounter/index.vue
+++ components/ytcounter/index.vue
@@ -2,7 +2,7 @@
   <v-flex xs2 offset-sm5 class="youtube-counter mt-1">
     <v-btn error dark medium tag='a' target='_blank' href='https://www.youtube.com/c/classicminidiy'>
       <v-icon fa class="white--text pr-2">youtube-play</v-icon>
-      {{ytData.meta.feed_details.channel_stats.subscriberCount}}
+      {{ytSubs}}
     </v-btn>
   </v-flex>
 </template>
@@ -10,18 +10,32 @@
 <style lang="scss" src="./ytcounter.scss"></style>
 
 <script>
+  import axios from 'axios';
+
   export default {
     data () {
-      const feed = 'https://app.hipposm.com/feed/5967b6eb552901318d554842';
-
-      this.$http.get(feed).then(response => {
-        this.ytData = response.body;
-      }, response => {
-        console.log('Error getting YT Data');
-      });
       return {
-        ytData: this.ytData
+        ytData: null,
+        ytSubs: null,
+        ytMeta: null
       };
+    },
+    created: function () {
+      this.fetchData();
+    },
+    methods: {
+      fetchData: function () {
+        const feed = 'https://app.hipposm.com/feed/5967b6eb552901318d554842';
+        const vueScope = this;
+        axios.get(feed).then(function (response) {
+          // Raw Youtube feed
+          vueScope.ytData = response.data;
+          // Raw Youtube meta feed
+          vueScope.ytMeta = response.data.meta;
+          // Subscriber Count for Youtube Channel
+          vueScope.ytSubs = response.data.meta.feed_details.channel_stats.subscriberCount;
+        });
+      }
     }
   };
 </script>
--- plugins/plugins.js
+++ plugins/plugins.js
@@ -1,8 +1,4 @@
 import Vue from 'vue';
 import Vuetify from 'vuetify';
 
-function runIt () {
-  Vue.use(Vuetify);
-}
-
-runIt();
+Vue.use(Vuetify);
