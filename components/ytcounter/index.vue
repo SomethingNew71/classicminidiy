dcbaf41b2663913fa8fc16f8b7bc0f62e151080e
Cole Gentry <Peapod2007@gmail.com>
2017-07-15 18:31:58 -0400

    :art: Brand new youtube counter component

--- components/ytcounter/index.vue
+++ components/ytcounter/index.vue
@@ -1,17 +1,27 @@
 <template>
-  <section class="youtube-counter">
-
-  </section>
+  <v-flex xs2 offset-sm5 class="youtube-counter mt-1">
+    <v-btn error dark medium tag='a' target='_blank' href='https://www.youtube.com/c/classicminidiy'>
+      <v-icon fa class="white--text pr-2">youtube-play</v-icon>
+      {{ytData.meta.feed_details.channel_stats.subscriberCount}}
+    </v-btn>
+  </v-flex>
 </template>
 
 <style lang="scss" src="./ytcounter.scss"></style>
 
 <script>
-export default {
-  data: function () {
-    return {
-      value: true
-    };
-  }
-};
+  export default {
+    data () {
+      const feed = 'https://app.hipposm.com/feed/5967b6eb552901318d554842';
+
+      this.$http.get(feed).then(response => {
+        this.ytData = response.body;
+      }, response => {
+        console.log('Error getting YT Data');
+      });
+      return {
+        ytData: this.ytData
+      };
+    }
+  };
 </script>
