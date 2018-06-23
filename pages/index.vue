e7e06b2b9779fac8f45284b91ba3d79cd7e55e08
Cole Gentry <Peapod2007@gmail.com>
2018-06-22 22:36:27 -0400

    :art::bug: Rearranges homepage and fixes bugs

--- pages/index.vue
+++ pages/index.vue
@@ -4,10 +4,10 @@
     <v-jumbotron class="hero" color="grey lighten-2">
       <v-container fill-height>
         <v-layout align-center>
-          <v-flex xs12 md6 class="text-xs-center py-3 px-5">
-            <img class="pt-5 text-xs-center" src="/img/wheel-logo-black.png" alt="Classic Mini DIY Logo image">
+          <v-flex xs12 md6 class="text-xs-center px-5">
+            <img class="pt-5 text-xs-center responsive-img" src="/img/wheel-logo-black-large.png" alt="Classic Mini DIY Logo image">
             <br>
-            <span class="headline">The best source of Classic Mini DIY videos and guides</span>
+            <span class="headline">The best source of Classic Mini DIY videos and guides since 2015</span>
             <!-- <v-btn large color="primary" class="mx-0">See more</v-btn> -->
           </v-flex>
           <v-flex hidden-sm-and-down md6>
@@ -46,46 +46,35 @@
     </v-tabs>
     <!-- PAGE CONTENTS BEGIN -->
     <v-container class="pt-2">
-      <v-layout>
-        <v-flex xs12 sm10 offset-sm1 md8 offset-md2 class="py-5 text-xs-center">
-          <h3 class="display-1 pb-3">
-            Welcome to Classic Mini DIY
-          </h3>
-          <p class="">
-            Classic Mini DIY started back in 2015 and since then has been creating DIY videos teaching all ages how to work on their Classic Mini Coopers. Classic Mini DIY is first and foremost is about creating DIY videos for new and inexperienced Classic Mini owners to use and reference when fixing their own cars. While we will always be making DIY videos Classic Mini DIY hopes to be the best place on the internet to find Classic Mini information.
-          </p>
-          <p>Above you can find links to all the resources Classic Mini DIY has to offer. If your interested in supporting the channel head to the links and products below for more information.</p>
-        </v-flex>
-      </v-layout>
-      <featuredProducts></featuredProducts>
-      <v-layout justify-center class="py-5">
-        <v-flex xs12 md10 class="pa-5 red lighten-2 text-xs-center">
-          <h3 class="white--text display-2">
-            Help Support the Channel
-          </h3>
-          <p class="white--text subheading">
-            Check out the classic mini DIY parts sponsor for all your parts needs in North America.
-          </p>
-          <v-btn color="red--text lighten-2" href="http://bit.ly/cmdiypatreon" :target="'_blank'" large><span class="icon pr-3 py-2 headline red--text lighten-2"><i class="fab fa-patreon"></i></span> Patreon</v-btn>
-        </v-flex>
-      </v-layout>
       <youtubeVideos></youtubeVideos>
+      <featuredProducts></featuredProducts>
       <div class="pt-5"></div>
-      <v-layout justify-center>
-        <v-flex xs12 md10 class="pa-5 seven-card">
-          <v-layout wrap>
-            <v-flex xs12 md8 class="pr-3 text-xs-center text-md-left">
-              <h3 class="white--text display-2">
-                Seven Mini Parts
-              </h3>
-              <p class="white--text subheading">
-                Check out the classic mini DIY parts sponsor for all your parts needs in North America.
-              </p>
-            </v-flex>
-            <v-flex xs12 md3 class="text-xs-center">
-              <v-btn color="red white--text">Seven Mini</v-btn>
-            </v-flex>
-          </v-layout>
+      <v-layout justify-space-around wrap>
+        <v-flex xs12 md6 class="pa-2 text-xs-center">
+          <div class="red lighten-2 pa-4">
+            <h3 class="white--text display-1">
+              Help Support the Channel
+            </h3>
+            <p class="white--text subheading pt-3">
+              Check out the classic mini DIY parts sponsor for all your parts needs in North America.
+            </p>
+            <v-btn color="red--text lighten-2" href="http://bit.ly/cmdiypatreon" :target="'_blank'" large>
+              <span class="icon pr-3 py-2 headline red--text lighten-2"><i class="fab fa-patreon"></i></span> Patreon
+            </v-btn>
+          </div>
+        </v-flex>
+        <v-flex xs12 md6 class="pa-2">
+          <div class="pa-4 seven-card text-xs-center">
+            <h3 class="white--text display-1">
+              Seven Mini Parts
+            </h3>
+            <p class="white--text subheading pt-3">
+              Check out the classic mini DIY parts sponsor for all your parts needs in North America.
+            </p>
+            <v-btn color="red white--text" href="http://www.7ent.com/?af=cmdiy1959" :target="'_blank'" large>
+              <span class="icon pr-3 py-2 headline lighten-2"><i class="fa fa-car"></i></span> Seven Mini
+            </v-btn>
+          </div>
         </v-flex>
       </v-layout>
       <div class="pt-5"></div>
@@ -146,21 +135,6 @@ export default {
       display: block;
       margin-left: auto;
       margin-right: auto;
-    }
-  }
-
-  .sponsors {
-    .seven-ent, .patreon, .merch {
-      margin: auto;
-    }
-    .seven-ent {
-      background-color: #D40C16 !important;
-    }
-    .patreon {
-      background-color: #F96854 !important;
-    }
-    .merch {
-      background-color: #25B7D4 !important;
     }
   }
 }
