65d04b04e333a700150188a68027674ad27d91fb
Cole Gentry <Peapod2007@gmail.com>
2018-06-25 22:13:24 -0400

    :racehorse: Componentizes the support section

---
+++ components/supportSection/index.vue
@@ -0,0 +1,30 @@
+<template>
+  <v-layout justify-space-around wrap>
+    <v-flex xs12 md6 class="pa-2 text-xs-center">
+      <div class="red lighten-2 pa-4">
+        <h3 class="white--text display-1">
+          Help Support the Channel
+        </h3>
+        <p class="white--text subheading pt-3">
+          Check out the classic mini DIY parts sponsor for all your parts needs in North America.
+        </p>
+        <v-btn color="red--text lighten-2" href="http://bit.ly/cmdiypatreon" :target="'_blank'" large>
+          <span class="icon pr-3 py-2 headline red--text lighten-2"><i class="fab fa-patreon"></i></span> Patreon
+        </v-btn>
+      </div>
+    </v-flex>
+    <v-flex xs12 md6 class="pa-2">
+      <div class="pa-4 seven-card text-xs-center">
+        <h3 class="white--text display-1">
+          Seven Mini Parts
+        </h3>
+        <p class="white--text subheading pt-3">
+          Check out the classic mini DIY parts sponsor for all your parts needs in North America.
+        </p>
+        <v-btn color="red white--text" href="http://www.7ent.com/?af=cmdiy1959" :target="'_blank'" large>
+          <span class="icon pr-3 py-2 headline lighten-2"><i class="fa fa-car"></i></span> Seven Mini
+        </v-btn>
+      </div>
+    </v-flex>
+  </v-layout>
+</template>
--- pages/index.vue
+++ pages/index.vue
@@ -35,34 +35,7 @@
       <youtubeVideos></youtubeVideos>
       <featuredProducts></featuredProducts>
       <div class="pt-5"></div>
-      <v-layout justify-space-around wrap>
-        <v-flex xs12 md6 class="pa-2 text-xs-center">
-          <div class="red lighten-2 pa-4">
-            <h3 class="white--text display-1">
-              Help Support the Channel
-            </h3>
-            <p class="white--text subheading pt-3">
-              Check out the classic mini DIY parts sponsor for all your parts needs in North America.
-            </p>
-            <v-btn color="red--text lighten-2" href="http://bit.ly/cmdiypatreon" :target="'_blank'" large>
-              <span class="icon pr-3 py-2 headline red--text lighten-2"><i class="fab fa-patreon"></i></span> Patreon
-            </v-btn>
-          </div>
-        </v-flex>
-        <v-flex xs12 md6 class="pa-2">
-          <div class="pa-4 seven-card text-xs-center">
-            <h3 class="white--text display-1">
-              Seven Mini Parts
-            </h3>
-            <p class="white--text subheading pt-3">
-              Check out the classic mini DIY parts sponsor for all your parts needs in North America.
-            </p>
-            <v-btn color="red white--text" href="http://www.7ent.com/?af=cmdiy1959" :target="'_blank'" large>
-              <span class="icon pr-3 py-2 headline lighten-2"><i class="fa fa-car"></i></span> Seven Mini
-            </v-btn>
-          </div>
-        </v-flex>
-      </v-layout>
+      <supportSection></supportSection>
       <div class="pt-5"></div>
     </v-container>
   </section>
@@ -72,6 +45,7 @@
 import youtubeVideos from '~/components/youtubeVideos';
 import getStarted from '~/components/getStarted';
 import featuredProducts from '~/components/featuredProducts';
+import supportSection from '~/components/supportSection';
 
 export default {
   data () {
@@ -121,7 +95,8 @@ export default {
   components: {
     youtubeVideos,
     getStarted,
-    featuredProducts
+    featuredProducts,
+    supportSection
   }
 };
 </script>
