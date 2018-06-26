a87fdff13e6b47c4c4e36c80242cfda52085a0ba
Cole Gentry <Peapod2007@gmail.com>
2018-06-26 19:24:40 -0400

    :racehorse: :art: Major optimization of electrical diagrams

--- pages/toolset/electrical.vue
+++ pages/toolset/electrical.vue
@@ -1,168 +1,34 @@
 <template>
-  <div class="electrical pt-4">
-    <v-flex xs12>
-      <img src="/icons/Multicolor/SVG/Round Icons/Battery-Charging.svg" alt="" width="70px" class="pb-3">
-      <h1 class="display-1 white--text">Wiring Diagrams</h1>
-      <p class="accent--text">Documents redrawn by Mike L on The Mini Forum</p>
-    </v-flex>
-    <v-flex xs12>
-      <v-divider></v-divider>
-    </v-flex>
-    <!-- POSITIVE GROUND SECTION -->
-    <v-flex xs12>
-      <v-toolbar color="primary" dark dense>
-        <v-icon fa class="fas white--text">plus</v-icon>
-        <v-toolbar-title color="white--text">Positive Ground</v-toolbar-title>
-      </v-toolbar>
-      <v-list dense>
-        <div :key="index" v-for="(diagram, index) in diagrams.positiveGround">
-          <v-list-tile avatar :href="diagram.link" target="_blank">
-            <v-list-tile-avatar>
-              <v-icon>picture_as_pdf</v-icon>
-            </v-list-tile-avatar>
-            <v-list-tile-content>
-              <v-list-tile-title>{{diagram.name}}</v-list-tile-title>
-              <v-list-tile-sub-title v-if="diagram.from || diagram.to">
-                {{diagram.from}} to {{diagram.to}}
-              </v-list-tile-sub-title>
-            </v-list-tile-content>
-          </v-list-tile>
-        </div>
-      </v-list>
-    </v-flex>
-    <!-- NEGATIVE GROUND SECTION -->
-    <v-flex xs12>
-      <v-toolbar color="primary" dark dense>
-        <v-icon fa class="fas white--text">minus</v-icon>
-        <v-toolbar-title>Negative Ground</v-toolbar-title>
-      </v-toolbar>
-      <v-list dense>
-        <div :key="index" v-for="(diagram, index) in diagrams.negativeGround">
-          <v-list-tile avatar :href="diagram.link" target="_blank">
-            <v-list-tile-avatar>
-              <v-icon>picture_as_pdf</v-icon>
-            </v-list-tile-avatar>
-            <v-list-tile-content>
-              <v-list-tile-title>{{diagram.name}}</v-list-tile-title>
-              <v-list-tile-sub-title v-if="diagram.from || diagram.to">
-                {{diagram.from}} to {{diagram.to}}
-              </v-list-tile-sub-title>
-            </v-list-tile-content>
-          </v-list-tile>
-        </div>
-      </v-list>
-    </v-flex>
-    <!-- BRITISH LEYLAND SECTION -->
-    <v-flex xs12>
-      <v-toolbar color="primary" dark dense>
-        <v-icon fa class="fas white--text">car</v-icon>
-        <v-toolbar-title>From British Leyland AKD 4935 (9th edition)</v-toolbar-title>
-      </v-toolbar>
-      <v-list dense>
-        <div :key="index" v-for="(diagram, index) in diagrams.britishLeyland">
-          <v-list-tile avatar :href="diagram.link" target="_blank">
-            <v-list-tile-avatar>
-              <v-icon>picture_as_pdf</v-icon>
-            </v-list-tile-avatar>
-            <v-list-tile-content>
-              <v-list-tile-title>{{diagram.name}}</v-list-tile-title>
-              <v-list-tile-sub-title v-if="diagram.from || diagram.to">
-                {{diagram.from}} to {{diagram.to}}
-              </v-list-tile-sub-title>
-            </v-list-tile-content>
-          </v-list-tile>
-        </div>
-      </v-list>
-    </v-flex>
-    <!-- EARLY ROVER SECTION -->
-    <v-flex xs12>
-      <v-toolbar color="primary" dark dense>
-        <v-icon fa class="fas white--text">car</v-icon>
-        <v-toolbar-title>From Rover Repair Manual AKM 6353</v-toolbar-title>
-      </v-toolbar>
-      <v-list dense>
-        <div :key="index" v-for="(diagram, index) in diagrams.earlyRover">
-          <v-list-tile avatar :href="diagram.link" target="_blank">
-            <v-list-tile-avatar>
-              <v-icon>picture_as_pdf</v-icon>
-            </v-list-tile-avatar>
-            <v-list-tile-content>
-              <v-list-tile-title>{{diagram.name}}</v-list-tile-title>
-              <v-list-tile-sub-title v-if="diagram.from || diagram.to">
-                {{diagram.from}} to {{diagram.to}}
-              </v-list-tile-sub-title>
-            </v-list-tile-content>
-          </v-list-tile>
-        </div>
-      </v-list>
-    </v-flex>
-    <!-- LATER ROVER SECTION -->
-    <v-flex xs12>
-      <v-toolbar color="primary" dark dense>
-        <v-icon fa class="fas white--text">car</v-icon>
-        <v-toolbar-title>From Rover repair manual AKM 7169</v-toolbar-title>
-      </v-toolbar>
-      <v-list dense>
-        <div :key="index" v-for="(diagram, index) in diagrams.laterRover">
-          <v-list-tile avatar :href="diagram.link" target="_blank">
-            <v-list-tile-avatar>
-              <v-icon>picture_as_pdf</v-icon>
-            </v-list-tile-avatar>
-            <v-list-tile-content>
-              <v-list-tile-title>{{diagram.name}}</v-list-tile-title>
-              <v-list-tile-sub-title v-if="diagram.from || diagram.to">
-                {{diagram.from}} to {{diagram.to}}
-              </v-list-tile-sub-title>
-            </v-list-tile-content>
-          </v-list-tile>
-        </div>
-      </v-list>
-    </v-flex>
-    <!-- HORNET ELF SECTION -->
-    <v-flex xs12>
-      <v-toolbar color="primary" dark dense>
-        <v-icon fa class="fas white--text">car</v-icon>
-        <v-toolbar-title>BMC Hornet and Elf  AKM 4062D</v-toolbar-title>
-      </v-toolbar>
-      <v-list dense>
-        <div :key="index" v-for="(diagram, index) in diagrams.hornetElf">
-          <v-list-tile avatar :href="diagram.link" target="_blank">
-            <v-list-tile-avatar>
-              <v-icon>picture_as_pdf</v-icon>
-            </v-list-tile-avatar>
-            <v-list-tile-content>
-              <v-list-tile-title>{{diagram.name}}</v-list-tile-title>
-              <v-list-tile-sub-title v-if="diagram.from || diagram.to">
-                {{diagram.from}} to {{diagram.to}}
-              </v-list-tile-sub-title>
-            </v-list-tile-content>
-          </v-list-tile>
-        </div>
-      </v-list>
-    </v-flex>
-    <!-- AUS MINI SECTION -->
-    <v-flex xs12>
-      <v-toolbar color="primary" dark dense>
-        <v-icon fa class="fas white--text">car</v-icon>
-        <v-toolbar-title>Australian Workshop Manual Supplement Publication TP 759 A</v-toolbar-title>
-      </v-toolbar>
-      <v-list dense>
-        <div :key="index" v-for="(diagram, index) in diagrams.ausMinis">
-          <v-list-tile avatar :href="diagram.link" target="_blank">
-            <v-list-tile-avatar>
-              <v-icon>picture_as_pdf</v-icon>
-            </v-list-tile-avatar>
-            <v-list-tile-content>
-              <v-list-tile-title>{{diagram.name}}</v-list-tile-title>
-              <v-list-tile-sub-title v-if="diagram.from || diagram.to">
-                {{diagram.from}} to {{diagram.to}}
-              </v-list-tile-sub-title>
-            </v-list-tile-content>
-          </v-list-tile>
-        </div>
-      </v-list>
-    </v-flex>
-  </div>
+  <v-container grid-list-xl>
+    <v-layout wrap fluid class="electrical">
+      <v-flex xs12>
+        <img src="/icons/Multicolor/SVG/Round Icons/Battery-Charging.svg" alt="" width="70px" class="pb-3">
+        <h1 class="display-1">Wiring Diagrams</h1>
+        <p>Documents redrawn by Mike L on The Mini Forum</p>
+      </v-flex>
+      <v-flex xs12 md6 :key="key" v-for="(groups, key) in diagrams">
+        <v-toolbar color="primary" dark dense>
+          <v-icon fa class="fas white--text">plus</v-icon>
+          <v-toolbar-title color="white--text">{{key}}</v-toolbar-title>
+        </v-toolbar>
+        <v-list dense>
+          <div :key="diagram" v-for="(diagram) in groups">
+            <v-list-tile avatar :href="diagram.link" target="_blank">
+              <v-list-tile-avatar>
+                <v-icon>picture_as_pdf</v-icon>
+              </v-list-tile-avatar>
+              <v-list-tile-content>
+                <v-list-tile-title>{{diagram.name}}</v-list-tile-title>
+                <v-list-tile-sub-title v-if="diagram.from || diagram.to">
+                  {{diagram.from}} to {{diagram.to}}
+                </v-list-tile-sub-title>
+              </v-list-tile-content>
+            </v-list-tile>
+          </div>
+        </v-list>
+      </v-flex>
+    </v-layout>
+  </v-container>
 </template>
 
 <style lang="scss">
@@ -190,7 +56,7 @@ export default {
   data: function () {
     return {
       diagrams: {
-        positiveGround: [
+        'Positive Ground': [
           {
             name: 'Mini Super/Super Deluxe/Countryman/Traveller/Cooper',
             from: 1959,
@@ -217,14 +83,14 @@ export default {
           },
           {
             name: 'Mini Super Deluxe/Countryman/Traveller/Cooper/Cooper S Mk2',
-            from: '',
-            to: '',
+            from: '1959',
+            to: '...',
             link: 'https://drive.google.com/open?id=0Bw6fNSzkJtQSeFlmQUdEd21ER1E'
           },
           {
             name: 'Mini Standard Saloon/Van/Pickup Mk2',
-            from: '',
-            to: '',
+            from: '1959',
+            to: '...',
             link: 'https://drive.google.com/open?id=0Bw6fNSzkJtQSX0V4Q1A2aXlPRzg'
           },
           {
@@ -235,70 +101,38 @@ export default {
           },
           {
             name: 'Mini Moke Mk2',
-            from: '',
-            to: '',
+            from: '1959',
+            to: '...',
             link: 'https://drive.google.com/open?id=0Bw6fNSzkJtQSc1VXMXpYNjNfX2c'
           }
         ],
-        negativeGround: [
+        'Negative Ground': [
           {
             name: 'Mini 850 Deluxe Saloon/Van/Pickup',
-            from: '',
-            to: '',
+            from: '1959',
+            to: '...',
             link: 'https://drive.google.com/open?id=0Bw6fNSzkJtQSMlR3eXp4N3hETUU'
           },
           {
             name: 'Mini 1000 Special Deluxe Saloon/Cooper S Mk3',
-            from: '',
-            to: '',
+            from: '1959',
+            to: '...',
             link: 'https://drive.google.com/open?id=0Bw6fNSzkJtQSZUJGREV6bllUc0U'
           },
           {
             name: 'Mini 1275 GT',
-            from: '',
-            to: '',
+            from: '1959',
+            to: '...',
             link: 'https://drive.google.com/open?id=0Bw6fNSzkJtQSUUJRdmJKWjg3YVU'
           },
           {
             name: 'Mini Clubman Saloon/Estate',
-            from: '',
-            to: '',
+            from: '1959',
+            to: '...',
             link: 'https://drive.google.com/open?id=0Bw6fNSzkJtQSRVlJMkJyUFltdGs'
           }
         ],
-        britishLeyland: [
-          {
-            name: 'Mini 1000 Saloon(Canada)',
-            from: '',
-            to: '1976',
-            link: 'https://drive.google.com/open?id=0Bw6fNSzkJtQSYmlGa0VTM2lhbjg'
-          },
-          {
-            name: 'Mini 850 Deluxe/Van/Pickup with Alternator and Rocker Switches',
-            from: '',
-            to: '1976',
-            link: 'https://drive.google.com/open?id=0Bw6fNSzkJtQSSFFqc0RPWHNfQ0U'
-          },
-          {
-            name: 'Mini 1000 Special Deluxe with Alternator and Rocker Switches',
-            from: '',
-            to: '1976',
-            link: 'https://drive.google.com/open?id=0Bw6fNSzkJtQSR1YtYnhObjROS3M'
-          },
-          {
-            name: 'Mini Clubman Saloon/Estate with Alternator and Rocker Switches',
-            from: '',
-            to: '1976',
-            link: 'https://drive.google.com/open?id=0Bw6fNSzkJtQSMW9GY0hSekxMQmM'
-          },
-          {
-            name: 'Mini 1275 GT with Alternator and Rocker Switches',
-            from: '',
-            to: '1976',
-            link: 'https://drive.google.com/open?id=0Bw6fNSzkJtQSLTZtN3RScHk3STQ'
-          }
-        ],
-        earlyRover: [
+        'Early Rover': [
           {
             name: 'Mini 850 Saloon/Van/Pickup',
             from: '1976',
@@ -343,24 +177,24 @@ export default {
           },
           {
             name: 'Twin Gauge Instrument Pack With Integral Voltage Stabilizer',
-            from: '',
+            from: '1959',
             to: '1989',
             link: 'https://drive.google.com/open?id=0Bw6fNSzkJtQSelFYYnQ2bEVDaFU'
           },
           {
             name: 'Triple Gauge Instrument Pack With Intergral Voltage Stabilizer',
-            from: '',
+            from: '1959',
             to: '1989',
             link: 'https://drive.google.com/open?id=0Bw6fNSzkJtQSQk11anBCVVY2bHc'
           },
           {
             name: 'Mini Turbo Model',
-            from: '',
+            from: '1959',
             to: '1989',
             link: 'https://drive.google.com/open?id=0Bw6fNSzkJtQSSGY3d3hPWnRZVmc'
           }
         ],
-        laterRover: [
+        'Later Rover': [
           {
             name: 'Power Distribution - Carburetter Models',
             from: '1992',
@@ -458,7 +292,39 @@ export default {
             link: 'https://drive.google.com/open?id=0Bw6fNSzkJtQSZElQLUpJVU1QRTA'
           }
         ],
-        hornetElf: [
+        'British Leyland': [
+          {
+            name: 'Mini 1000 Saloon(Canada)',
+            from: '1959',
+            to: '1976',
+            link: 'https://drive.google.com/open?id=0Bw6fNSzkJtQSYmlGa0VTM2lhbjg'
+          },
+          {
+            name: 'Mini 850 Deluxe/Van/Pickup with Alternator and Rocker Switches',
+            from: '1959',
+            to: '1976',
+            link: 'https://drive.google.com/open?id=0Bw6fNSzkJtQSSFFqc0RPWHNfQ0U'
+          },
+          {
+            name: 'Mini 1000 Special Deluxe with Alternator and Rocker Switches',
+            from: '1959',
+            to: '1976',
+            link: 'https://drive.google.com/open?id=0Bw6fNSzkJtQSR1YtYnhObjROS3M'
+          },
+          {
+            name: 'Mini Clubman Saloon/Estate with Alternator and Rocker Switches',
+            from: '1959',
+            to: '1976',
+            link: 'https://drive.google.com/open?id=0Bw6fNSzkJtQSMW9GY0hSekxMQmM'
+          },
+          {
+            name: 'Mini 1275 GT with Alternator and Rocker Switches',
+            from: '1959',
+            to: '1976',
+            link: 'https://drive.google.com/open?id=0Bw6fNSzkJtQSLTZtN3RScHk3STQ'
+          }
+        ],
+        'Hornet Elf': [
           {
             name: 'Hornet and Elf',
             from: '1961',
@@ -478,35 +344,35 @@ export default {
             link: 'https://drive.google.com/open?id=0Bw6fNSzkJtQSS3E2MmpHa2F0aUE'
           }
         ],
-        ausMinis: [
+        'Aus Minis': [
           {
             name: 'Australian Standard Saloon/Van',
             from: '1969',
-            to: '',
+            to: '...',
             link: 'https://drive.google.com/open?id=0Bw6fNSzkJtQSZ1BZdi1aYXg3Q28'
           },
           {
             name: 'Australian BMC Moke with Alternator',
             from: '1969',
-            to: '',
+            to: '...',
             link: 'https://drive.google.com/open?id=0Bw6fNSzkJtQSVHZRYUNHb2FFTGs'
           },
           {
             name: 'Australian Mini and BMC Moke with Generator',
             from: '1969',
-            to: '',
+            to: '...',
             link: 'https://drive.google.com/open?id=0Bw6fNSzkJtQSSzEtMEhZY3RTa2M'
           },
           {
             name: 'Australian Mini 1000k/1100STD/Van/Mini Matic(1100)/Cooper S(Alternator)',
             from: '1969',
-            to: '',
+            to: '...',
             link: 'https://drive.google.com/open?id=0Bw6fNSzkJtQSQnpqeHBGeEhzMm8'
           },
           {
             name: 'Australian Mini Cooper S with Generator',
             from: '1969',
-            to: '',
+            to: '...',
             link: 'https://drive.google.com/open?id=0Bw6fNSzkJtQSOW44empPZndCYlE'
           }
         ]
