bc6119860a27be7bb5c2c9fcd85b168d9ed8b1ec
Cole Gentry <Peapod2007@gmail.com>
2019-01-19 22:50:35 -0500

    Updates stuff

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,24 +1,28 @@
-module.exports = {
-  extends: 'standard',
+export default {
+  root: true,
   env: {
     browser: true,
     node: true
   },
   parserOptions: {
-    // ecmaFeatures: {
-    //   classes: true
-    // }
+    parser: 'babel-eslint'
   },
-  parser: 'babel-eslint',
-  root: true,
+  extends: [
+    "eslint:recommended",
+    // https://github.com/vuejs/eslint-plugin-vue#priority-a-essential-error-prevention
+    // consider switching to `plugin:vue/strongly-recommended` or `plugin:vue/recommended` for stricter rules.
+    "plugin:vue/recommended",
+    "plugin:prettier/recommended"
+  ],
   // required to lint *.vue files
   plugins: [
-    'html',
-    // 'import'
+    'vue'
   ],
   // add your custom rules here
   rules: {
-    semi: [ "error", "always" ]
-  },
-  globals: {}
+    "semi": [2, "never"],
+    "no-console": "off",
+    "vue/max-attributes-per-line": "off",
+    "prettier/prettier": ["error", { "semi": false }]
+  }
 }
