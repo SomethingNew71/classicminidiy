a31a62f356910ebf4b448a64fdcfde8711d382d1
Cole Gentry <Peapod2007@gmail.com>
2019-04-02 20:24:46 -0400

    Establishes new baseline for entire app

---
+++ nuxt.config.js
@@ -0,0 +1,81 @@
+import pkg from './package'
+
+export default {
+  mode: 'universal',
+
+  /*
+   ** Headers of the page
+   */
+  head: {
+    title: pkg.name,
+    meta: [
+      { charset: 'utf-8' },
+      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+      { hid: 'description', name: 'description', content: pkg.description }
+    ],
+    link: [
+      { rel: 'icon', type: 'image/png', sizes: '96x96', href: '/favicon/favicon-96x96.png' },
+      { rel: 'icon', type: 'image/png', sizes: '32x32', href: '/favicon/favicon-32x32.png' },
+      { rel: 'icon', type: 'image/png', sizes: '16x16', href: '/favicon/favicon-16x16.png' },
+      { rel: 'icon', type: 'image/x-icon', href: '/icons/preview.png' },
+      { rel: 'image_src', href: '/static' }
+    ],
+    script: [
+      { async: true, src: '//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js' },
+      { defer: true, src: 'https://pro.fontawesome.com/releases/v5.0.13/js/all.js', integrity: 'sha384-d84LGg2pm9KhR4mCAs3N29GQ4OYNy+K+FBHX8WhimHpPm86c839++MDABegrZ3gn', crossorigin: 'anonymous' },
+      { defer: true, src: 'https://use.fontawesome.com/releases/v5.0.13/js/v4-shims.js' }
+    ]
+  },
+
+  /*
+   ** Customize the progress-bar color
+   */
+  loading: { color: '#fff' },
+
+  /*
+   ** Global CSS
+   */
+  css: [],
+
+  /*
+   ** Plugins to load before mounting the App
+   */
+  plugins: [],
+
+  /*
+   ** Nuxt.js modules
+   */
+  modules: [
+    // Doc: https://axios.nuxtjs.org/usage
+    '@nuxtjs/axios',
+    // Doc: https://buefy.github.io/#/documentation
+    'nuxt-buefy',
+    ['@nuxtjs/google-analytics', { ua: 'UA-53914731-2' }]
+  ],
+  /*
+   ** Axios module configuration
+   */
+  axios: {
+    // See https://github.com/nuxt-community/axios-module#options
+  },
+
+  /*
+   ** Build configuration
+   */
+  build: {
+    /*
+     ** You can extend webpack config here
+     */
+    extend(config, ctx) {
+      // Run ESLint on save
+      if (ctx.isDev && ctx.isClient) {
+        config.module.rules.push({
+          enforce: 'pre',
+          test: /\.(js|vue)$/,
+          loader: 'eslint-loader',
+          exclude: /(node_modules)/
+        })
+      }
+    }
+  }
+}
