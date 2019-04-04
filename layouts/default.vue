cb19a2064d06f0d68299fe1b15c9ef9e20f9c372
Cole Gentry <Peapod2007@gmail.com>
2019-04-03 22:03:40 -0400

    Finalizes base landing page for CMDIY

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,45 +1,71 @@
 <template>
-  <div>
-    <hero></hero>
-    <section class="main-content full-width">
-      <div class="container column is-12">
-        <nuxt />
+  <section class="main-content full-width">
+    <nuxt />
+    <footer class="footer has-background-dark has-text-light">
+      <div class="content has-text-centered">
+        <p>
+          <strong class="has-text-light">Classic Mini DIY</strong> by <a class="has-text-info" href="https://github.com/SomethingNew71">Cole Gentry</a>. The source code is licensed
+          <a class="has-text-info" href="http://opensource.org/licenses/mit-license.php">MIT</a>. The website content
+          is licensed <a class="has-text-info" href="http://creativecommons.org/licenses/by-nc-sa/4.0/">CC BY NC SA 4.0</a>.
+        </p>
+        <p>Sign up for the Classic Mini DIY Newsletter</p>
+        <div>
+          <!-- Begin Mailchimp Signup Form -->
+          <div id="mc_embed_signup">
+            <link href="//cdn-images.mailchimp.com/embedcode/slim-10_7.css" rel="stylesheet" type="text/css">
+            <form
+              action="https://classicminidiy.us16.list-manage.com/subscribe/post?u=8c4cae17c7a50111c3fe1a26a&amp;id=6a5daeee77"
+              method="post"
+              id="mc-embedded-subscribe-form"
+              name="mc-embedded-subscribe-form"
+              class="validate"
+              target="_blank"
+              novalidate>
+              <div id="mc_embed_signup_scroll">
+                <input
+                  type="email"
+                  value=""
+                  name="EMAIL"
+                  class="email"
+                  id="mce-EMAIL"
+                  placeholder="Email Address"
+                  required>
+                  <!-- real people should not fill this in and expect good things - do not remove this or risk form bot signups-->
+                  <div style="position: absolute; left: -5000px;" aria-hidden="true">
+                    <input type="text" name="b_8c4cae17c7a50111c3fe1a26a_6a5daeee77" tabindex="-1" value="">
+                  </div>
+                  <div class="clear">
+                    <input
+                      type="submit"
+                      value="Subscribe"
+                      name="subscribe"
+                      id="mc-embedded-subscribe"
+                      class="button">
+                  </div>
+              </div>
+            </form>
+          </div>
+          <!--End mc_embed_signup-->
+        </div>
       </div>
-    </section>
-  </div>
+    </footer>
+  </section>
 </template>
 
-<script>
-import hero from '~/components/Hero'
-export default {
-  data() {
-    return {
-      items: [
-        {
-          title: 'About',
-          icon: 'home',
-          to: { name: 'about' }
-        },
-        {
-          title: 'Videos',
-          icon: 'home',
-          to: { name: 'videos' }
-        },
-        {
-          title: 'Merch',
-          icon: 'home',
-          to: { name: 'merch' }
-        },
-        {
-          title: 'Support Me',
-          icon: 'lightbulb',
-          to: { name: 'support' }
-        }
-      ]
+<style lang="scss" scoped>
+  #mc_embed_signup {
+    form {
+      padding: 0;
+      input {
+        height: inherit;
+        margin-top: 10px;
+        margin-left: auto;
+        margin-right: auto;
+        max-width: 400px;
+      }
+      #mc-embedded-subscribe {
+        max-width: 100px;
+      }
     }
-  },
-  components: {
-    hero
   }
-}
-</script>
+</style>
