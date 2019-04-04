39497059e7a4ad3926092330258041f341816f74
Cole Gentry <Peapod2007@gmail.com>
2019-04-03 22:03:40 -0400

    Finalizes base landing page for CMDIY

--- components/Hero.vue
+++ components/Hero.vue
@@ -1,54 +1,81 @@
 <template>
   <section class="hero is-info is-bold is-medium">
-    <div class="hero-head">
-      <nav class="navbar">
-        <div class="container">
-          <div id="" class="navbar-menu">
-            <div class="navbar-end">
-
-              <span class="navbar-item">
-
-              </span>
-            </div>
-          </div>
-        </div>
-      </nav>
-    </div>
-
     <div class="hero-body">
       <div class="container has-text-centered">
         <p class="title">
-          <img src="/img/wheel-logo.png" alt>
+          <img src="/img/logo.png" alt>
         </p>
         <p class="subtitle">
-          <a alt="instagram" class="hero-item">
+          The best source for DIY knowledge on your Classic Mini Cooper
+        </p>
+        <p class="subtitle">
+          <a alt="instagram" class="hero-item" href="https://instagram.com/classicminidiy59">
             <span class="icon is-large">
               <i class="fab fa-2x fa-instagram"></i>
             </span>
           </a>
-          <a class="hero-item">
+          <a alt="Patreon" class="hero-item" href="https://patreon.com/classicminidiy">
+            <span class="icon is-large">
+              <i class="fab fa-2x fa-patreon"></i>
+            </span>
+          </a>
+          <a alt="Facebook" class="hero-item" href="https://facebook.com/classicminidiy">
             <span class="icon is-large">
               <i class="fab fa-2x fa-facebook-f"></i>
             </span>
           </a>
-          <a class="hero-item">
+          <a alt="Youtube" class="hero-item" href="https://youtube.com/c/classicminidiy">
             <span class="icon is-large">
               <i class="fab fa-2x fa-youtube"></i>
             </span>
           </a>
         </p>
-        <p>
-          <a class="button is-medium is-youtube" target='_blank' href='https://www.youtube.com/c/classicminidiy?sub_confirmation=1'>
+        <p class="subtitle">
+          <a class="button is-youtube" target='_blank' href='https://www.youtube.com/c/classicminidiy?sub_confirmation=1'>
             <span class="icon">
               <i class="fab fa-youtube"></i>
             </span>
-            <span>{{ youtubeSubs }} - Subscribe</span>
+            <span>Subscribe</span>
           </a>
         </p>
       </div>
     </div>
+    <div class="hero-foot">
+      <nav class="level is-mobile">
+        <div class="level-item has-text-centered">
+          <div>
+            <p class="heading">Producing Videos for</p>
+            <p class="title">4 years</p>
+          </div>
+        </div>
+        <div class="level-item has-text-centered">
+          <div>
+            <p class="heading">Videos</p>
+            <p class="title">{{videos}}</p>
+          </div>
+        </div>
+        <div class="level-item has-text-centered">
+          <div>
+            <p class="heading">Subscribers</p>
+            <p class="title">{{subscribers}}</p>
+          </div>
+        </div>
+        <div class="level-item has-text-centered">
+          <div>
+            <p class="heading">Total Views</p>
+            <p class="title">{{views}}</p>
+          </div>
+        </div>
+      </nav>
+    </div>
   </section>
 </template>
+
+<style lang="scss" scoped>
+ .hero-foot {
+   padding-bottom: 10px;
+ }
+</style>
 
 <script>
 import axios from 'axios'
@@ -56,24 +83,27 @@ import axios from 'axios'
 export default {
   data() {
     return {
-      youtubeSubs: null
+      subscribers: null,
+      videos: null,
+      views: null
     }
   },
-  created: function () {
+  created() {
     this.fetchData()
   },
   methods: {
-    fetchData: function () {
+    fetchData() {
       const apiKey = 'AIzaSyCbmPC-gSOA2g9xQOAeBboTTwz4tSVRQZ8'
       const baseURL = 'https://www.googleapis.com/youtube/v3/channels'
       const channelId = 'UCZIUfOFhrQ9nrR06IOoAJ2Q'
       const details = 'statistics'
       const feed = `${baseURL}?key=${apiKey}&id=${channelId}&part=${details}`
       const vueScope = this
-      axios.get(feed).then(function (response) {
+      axios.get(feed).then((response) => {
         // Subscriber Count for Youtube Channel
-        vueScope.youtubeSubs =
-          response.data.items[0].statistics.subscriberCount
+        vueScope.subscribers = new Intl.NumberFormat().format(response.data.items[0].statistics.subscriberCount)
+        vueScope.videos = new Intl.NumberFormat().format(response.data.items[0].statistics.videoCount)
+        vueScope.views = new Intl.NumberFormat().format(response.data.items[0].statistics.viewCount)
       })
     }
   }
--- pages/index.vue
+++ pages/index.vue
@@ -1,30 +1,83 @@
 <template>
-  <section class="section">
-    <div class="columns is-mobile">
-      <card title="DIY Videos" icon="github-circle">
-        Open source on <a href="https://github.com/buefy/buefy"> GitHub</a>
-      </card>
-
-      <card title="Online Store" icon="cellphone-link">
-        <b class="has-text-grey">Every</b> component is responsive
-      </card>
-
-      <card title="Instagram" icon="alert-decagram">
-        Built with <a href="https://vuejs.org/">Vue.js</a> and
-        <a href="http://bulma.io/">Bulma</a>
-      </card>
+  <div>
+    <hero/>
+    <div class="container column is-12">
+      <section class="section">
+        <div class="columns">
+          <div class="column is-4" v-for="card in cards" :key="card">
+            <div class="card">
+              <div class="card-image">
+                <figure class="image is-4by3">
+                  <img :src="card.image" alt="Placeholder image">
+                </figure>
+              </div>
+              <header class="card-header">
+                <p class="card-header-title">
+                  {{card.title}}
+                </p>
+              </header>
+              <div class="card-content">
+                <div class="content">
+                  {{ card.body }}
+                </div>
+              </div>
+              <footer class="card-footer">
+                <a v-for="link in card.footerLinks" :key="link" :href="link.url" class="card-footer-item has-text-info">{{link.title}}</a>
+              </footer>
+            </div>
+          </div>
+        </div>
+      </section>
     </div>
-  </section>
+  </div>
 </template>
 
 <script>
-import Card from '~/components/Card'
+import hero from '~/components/Hero'
 
 export default {
   name: 'HomePage',
-
+  data() {
+    return {
+      cards: {
+        0: {
+          title: 'DIY Videos',
+          body: 'The primary work of Classic Mini DIY is creating videos outlining the way to work on your Classic Mini Cooper.',
+          image: '/img/camera.gif',
+          footerLinks: [
+            {
+              title: 'Start Watching',
+              url: 'https://www.youtube.com/c/classicminidiy?sub_confirmation=1'
+            }
+          ]
+        },
+        1: {
+          title: 'Practical Approach to DIY',
+          body: 'By making these videos, we hope to keep the Classic Mini alive, as well as keeping the entire online community thriving for years to come.',
+          image: '/img/mini.gif',
+          footerLinks: [
+            {
+              title: 'Learn More',
+              url: 'https://www.youtube.com/c/classicminidiy?sub_confirmation=1'
+            }
+          ]
+        },
+        2: {
+          title: 'Mail in Repair Service',
+          body: 'A recent addition to our lineup, is the option to send your old or broken Classic Mini parts for a top notch repair and recondition service.',
+          image: '/img/tools.gif',
+          footerLinks: [
+            {
+              title: 'Email for details',
+              url: 'mailto:classicminidiy@gmail.com'
+            }
+          ]
+        }
+      }
+    }
+  },
   components: {
-    Card
+    hero
   }
 }
 </script>
