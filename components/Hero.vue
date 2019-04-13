a6d1b42c30dc8f5d7dda7d90a140ec064a75552d
Cole Gentry <Peapod2007@gmail.com>
2019-04-12 22:02:52 -0400

    Linting fixes and a few bug fixes to the layout

--- components/Hero.vue
+++ components/Hero.vue
@@ -9,61 +9,77 @@
           The best source for DIY knowledge on your Classic Mini Cooper
         </p>
         <p class="subtitle">
-          <a alt="instagram" class="hero-item" href="https://instagram.com/classicminidiy59">
+          <a alt="instagram" class="hero-item" target="_blank" href="https://instagram.com/classicminidiy59">
             <span class="icon is-large">
-              <i class="fab fa-2x fa-instagram"></i>
+              <i class="fab fa-2x fa-instagram" />
             </span>
           </a>
-          <a alt="Patreon" class="hero-item" href="https://patreon.com/classicminidiy">
+          <a alt="Patreon" class="hero-item" target="_blank" href="https://patreon.com/classicminidiy">
             <span class="icon is-large">
-              <i class="fab fa-2x fa-patreon"></i>
+              <i class="fab fa-2x fa-patreon" />
             </span>
           </a>
-          <a alt="Facebook" class="hero-item" href="https://facebook.com/classicminidiy">
+          <a alt="Facebook" class="hero-item" target="_blank" href="https://facebook.com/classicminidiy">
             <span class="icon is-large">
-              <i class="fab fa-2x fa-facebook-f"></i>
+              <i class="fab fa-2x fa-facebook-f" />
             </span>
           </a>
-          <a alt="Youtube" class="hero-item" href="https://youtube.com/c/classicminidiy">
+          <a alt="Youtube" class="hero-item" target="_blank" href="https://youtube.com/c/classicminidiy">
             <span class="icon is-large">
-              <i class="fab fa-2x fa-youtube"></i>
+              <i class="fab fa-2x fa-youtube" />
             </span>
           </a>
         </p>
         <p class="subtitle">
-          <a class="button is-youtube" target='_blank' href='https://www.youtube.com/c/classicminidiy?sub_confirmation=1'>
+          <a class="button is-youtube" target="_blank" href="https://www.youtube.com/c/classicminidiy?sub_confirmation=1">
             <span class="icon">
-              <i class="fab fa-youtube"></i>
+              <i class="fab fa-youtube" />
             </span>
-            <span>Subscribe</span>
+            <span>Subscribe Today</span>
           </a>
         </p>
       </div>
     </div>
     <div class="hero-foot">
-      <nav class="level is-mobile">
+      <nav class="level">
         <div class="level-item has-text-centered">
           <div>
-            <p class="heading">Producing Videos for</p>
-            <p class="title">4 years</p>
+            <p class="heading">
+              Producing Videos for
+            </p>
+            <p class="title">
+              4 years
+            </p>
           </div>
         </div>
         <div class="level-item has-text-centered">
           <div>
-            <p class="heading">Videos</p>
-            <p class="title">{{videos}}</p>
+            <p class="heading">
+              Videos
+            </p>
+            <p class="title">
+              {{ videos }}
+            </p>
           </div>
         </div>
         <div class="level-item has-text-centered">
           <div>
-            <p class="heading">Subscribers</p>
-            <p class="title">{{subscribers}}</p>
+            <p class="heading">
+              Subscribers
+            </p>
+            <p class="title">
+              {{ subscribers }}
+            </p>
           </div>
         </div>
         <div class="level-item has-text-centered">
           <div>
-            <p class="heading">Total Views</p>
-            <p class="title">{{views}}</p>
+            <p class="heading">
+              Total Views
+            </p>
+            <p class="title">
+              {{ views }}
+            </p>
           </div>
         </div>
       </nav>
@@ -78,7 +94,7 @@
 </style>
 
 <script>
-import axios from 'axios'
+import axios from 'axios';
 
 export default {
   data() {
@@ -86,28 +102,28 @@ export default {
       subscribers: null,
       videos: null,
       views: null
-    }
+    };
   },
   created() {
-    this.fetchData()
+    this.fetchData();
   },
   methods: {
     fetchData() {
-      const apiKey = 'AIzaSyCbmPC-gSOA2g9xQOAeBboTTwz4tSVRQZ8'
-      const baseURL = 'https://www.googleapis.com/youtube/v3/channels'
-      const channelId = 'UCZIUfOFhrQ9nrR06IOoAJ2Q'
-      const details = 'statistics'
-      const feed = `${baseURL}?key=${apiKey}&id=${channelId}&part=${details}`
-      const vueScope = this
+      const apiKey = 'AIzaSyCbmPC-gSOA2g9xQOAeBboTTwz4tSVRQZ8';
+      const baseURL = 'https://www.googleapis.com/youtube/v3/channels';
+      const channelId = 'UCZIUfOFhrQ9nrR06IOoAJ2Q';
+      const details = 'statistics';
+      const feed = `${baseURL}?key=${apiKey}&id=${channelId}&part=${details}`;
+      const vueScope = this;
       axios.get(feed).then((response) => {
         // Subscriber Count for Youtube Channel
-        vueScope.subscribers = new Intl.NumberFormat().format(response.data.items[0].statistics.subscriberCount)
-        vueScope.videos = new Intl.NumberFormat().format(response.data.items[0].statistics.videoCount)
-        vueScope.views = new Intl.NumberFormat().format(response.data.items[0].statistics.viewCount)
-      })
+        vueScope.subscribers = new Intl.NumberFormat().format(response.data.items[0].statistics.subscriberCount);
+        vueScope.videos = new Intl.NumberFormat().format(response.data.items[0].statistics.videoCount);
+        vueScope.views = new Intl.NumberFormat().format(response.data.items[0].statistics.viewCount);
+      });
     }
   }
-}
+};
 </script>
 
 <style lang='scss' scoped>
