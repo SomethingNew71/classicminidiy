ddca9fcf1ce938a0ee853e179434b587ff9e3e38
Cole Gentry <Peapod2007@gmail.com>
2019-06-20 21:22:26 -0400

    Implements torque spec page and starts main tech page

--- pages/technical.vue
+++ pages/technical.vue
@@ -1,6 +1,55 @@
 <template>
   <div>
-    FARTS
+        <section class="hero hero-technical is-medium">
+      <!-- Hero head: will stick at the top -->
+      <div class="hero-head">
+        <header class="navbar">
+          <div class="container">
+            <div class="navbar-brand">
+              <span class="navbar-burger burger" data-target="navbarMenuHeroC">
+                <span></span>
+                <span></span>
+                <span></span>
+              </span>
+            </div>
+            <div id="navbarMenuHeroC" class="navbar-menu">
+              <div class="navbar-end">
+                <nuxt-link class="fancy-font-heavy has-text-white navbar-item" to="/">Home</nuxt-link>
+                <nuxt-link class="fancy-font-heavy has-text-white navbar-item" to="/technical">Technical Info</nuxt-link>
+                <a class="fancy-font-heavy has-text-white navbar-item" target="_blank" href="https://merch.classicminidiy.com">Store</a>
+                <span class="fancy-font-heavy navbar-item">
+                  <a class="button is-inverted is-danger" target='_blank' href='https://www.youtube.com/c/classicminidiy?sub_confirmation=1'>
+                    <span class="icon">
+                      <i class="fab fa-youtube"></i>
+                    </span>
+                    <span>Subscribe</span>
+                  </a>
+                </span>
+              </div>
+            </div>
+          </div>
+        </header>
+      </div>
+      <div class="hero-body">
+        <div class="container has-text-centered">
+          <p class="subtitle is-6 has-text-white">
+            YOUR DIGITAL MINI TOOLBOX
+          </p>
+          <h1 class="title fancy-font-bold has-text-white">
+            Technical Information
+          </h1>
+        </div>
+      </div>
+    </section>
+    <div class="container">
+      <section class="section">
+        <div class="columns">
+          <div class="column">
+            <nuxt-child />
+          </div>
+        </div>
+      </section>
+    </div>
   </div>
 </template>
 <script>
@@ -10,3 +59,9 @@ export default {
   }
 }
 </script>
+<style lang="scss">
+.hero-technical {
+  background: #2f2f2f url('/technical.jpg') no-repeat center;
+  background-size: cover;
+}
+</style>
---
+++ pages/technical/index.vue
@@ -0,0 +1,46 @@
+<template>
+  <section class="section">
+    <div class="columns">
+      <div v-for="(card, index) in cards" :key="index" class="column">
+        <div class="card">
+          <div class="card-image">
+            <figure class="image">
+              <img src="https://bulma.io/images/placeholders/1280x960.png" alt="Placeholder image">
+            </figure>
+          </div>
+          <div class="card-content">
+            <p class="title is-4 has-text-centered">{{ card.title }}</p>
+            <div class="content">
+              {{ card.aside }}
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
+</template>
+<script>
+export default {
+  data() {
+    return {
+      cards: [
+        {
+          title: 'Torque Specs',
+          image: '',
+          aside: ''
+        },
+        {
+          title: 'Manuals',
+          image: '',
+          aside: ''
+        },
+        {
+          title: 'Electrical Diagrams',
+          image: '',
+          aside: ''
+        }
+      ]
+    }
+  }
+}
+</script>
