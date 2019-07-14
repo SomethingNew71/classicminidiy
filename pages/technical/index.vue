075cba6b0f3559e201b273df2e7c45781bf2dfb7
Cole Gentry <Peapod2007@gmail.com>
2019-07-14 15:48:59 -0400

    Implements better behavior on mobile tech page
    Signed-off-by: Cole Gentry <Peapod2007@gmail.com>

--- pages/technical/index.vue
+++ pages/technical/index.vue
@@ -26,7 +26,7 @@
         <h4 class="fancy-font-bold is-size-3">Technical Information for the DIY Mechanic</h4>
         <p class="is-size-5  pt-20">One of the most important parts of Classic Mini DIY is the focus on getting out and working on your own car. So to make this easier for you, I have collected technical information from various sources and consolidated it right here on classicminidiy.com.</p>
       </div>
-      <div v-for="(card, index) in cards" :key="index" class="column">
+      <div v-for="(card, index) in cards" :key="index" class="column is-hidden-mobile">
         <nuxt-link :to="card.to">
           <div class="card">
             <div class="card-image">
@@ -36,12 +36,26 @@
             </div>
             <div class="card-content">
               <p class="title is-4 has-text-centered">{{ card.title }}</p>
-              <div class="content">
-                {{ card.aside }}
-              </div>
             </div>
           </div>
         </nuxt-link>
+      </div>
+      <div class="mobile-list column is-12 is-hidden-tablet">
+        <nav class="panel">
+          <p class="panel-heading">
+            Classic Mini Toolbox
+          </p>
+          <nuxt-link
+            class="panel-block is-active"
+            v-for="(card, index) in cards"
+            :key="index"
+            :to="card.to">
+            <span class="panel-icon">
+              <img :src="card.image">
+            </span>
+            {{ card.title }}
+          </nuxt-link>
+        </nav>
       </div>
     </div>
   </section>
@@ -54,25 +68,21 @@ export default {
         {
           title: 'Torque Specs',
           image: '/icons/Settings-5.svg',
-          aside: '',
           to: '/technical/torque'
         },
         {
           title: 'Manuals',
           image: '/icons/Checklist.svg',
-          aside: '',
           to: '/technical/manuals'
         },
         {
           title: 'Electrical Diagrams',
           image: '/icons/Battery-Charging.svg',
-          aside: '',
           to: '/technical/electrical'
         },
         {
           title: 'Wheel Dictionary',
           image: '/icons/Bus.svg',
-          aside: '',
           to: '/technical/wheels'
         }
       ]
@@ -85,5 +95,9 @@ export default {
 .icon-image {
   max-width: 50%;
   margin: auto;
+}
+.panel-icon {
+  height: 3em;
+  width: 3em;
 }
 </style>
