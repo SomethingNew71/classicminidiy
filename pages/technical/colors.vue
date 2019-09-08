84c74c1779c2dba61a2fa9c102269de0250a6d3d
Cole Gentry <Peapod2007@gmail.com>
2019-09-08 15:19:03 -0400

    Implenents data and base page scaffold for color picker
    Signed-off-by: Cole Gentry <Peapod2007@gmail.com>

---
+++ pages/technical/colors.vue
@@ -0,0 +1,46 @@
+<template>
+  <div>
+    <p>COLORS PAGE</p>
+  </div>
+</template>
+<script>
+export default {
+  data () {
+    return {
+      title: 'Technical Information',
+      subtitle: 'YOUR DIGITAL MINI TOOLBOX',
+      background: '/technical.jpg',
+      size: 'is-medium',
+      centered: true
+    }
+  },
+  methods: {
+    getColors () {
+      /*
+      // This is the order the colors appear in the JSON object
+      "columns": [
+        "colour_id",
+        "base_id",
+        "colour_code",
+        "colour_name",
+        "colour_short",
+        "colour_years",
+        "colour_swatch",
+        "colour_approved"
+      ],
+
+      // This is the color image URL
+      http://mini-colours.co.uk/images/swatches/${colour[2]}.jpg
+
+      // This is the no color found URL
+      http://mini-colours.co.uk/images/swatches/no_swatch.jpg
+    */
+    }
+  },
+  head () {
+    return {
+      title: 'Technical - Color Picker'
+    }
+  }
+}
+</script>
