81635dd91c6582fada9209bea10162778b3c49b9
Cole Gentry <Peapod2007@gmail.com>
2019-09-26 13:46:03 -0400

    Fixes title issue with iframe

--- pages/index.vue
+++ pages/index.vue
@@ -94,7 +94,7 @@
           <div class="column is-5">
             <style>.embed-container { position: relative; padding-bottom: 56.25%; height: 0; overflow: hidden; max-width: 100%; } .embed-container iframe, .embed-container object, .embed-container embed { position: absolute; top: 0; left: 0; width: 100%; height: 100%; }</style>
             <div class="embed-container">
-              <iframe src="https://www.youtube.com/embed//U9tBae3riug" frameborder="0" allowfullscreen />
+              <iframe title="Youtube Welcome Video" src="https://www.youtube.com/embed//U9tBae3riug" frameborder="0" allowfullscreen />
             </div>
           </div>
         </div>
