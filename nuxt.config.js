8cbc01ef5fcd2e28adfaa5b48bb9227634766787
Cole Gentry <Peapod2007@gmail.com>
2020-03-17 15:48:07 -0400

    Re-implements image optimizer on build after reviewing npm audit
    Signed-off-by: Cole Gentry <Peapod2007@gmail.com>

--- nuxt.config.js
+++ nuxt.config.js
@@ -59,7 +59,8 @@ export default {
     id: 'UA-53914731-2'
   },
   optimizedImages: {
-    optimizeImages: true
+    optimizeImages: true,
+    optimizeImagesInDev: true
   },
   // buefy: {
   //   materialDesignIcons: false
