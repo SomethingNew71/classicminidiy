321c01d82e544782127de0ccfae27f3ab56bd1bf
Cole Gentry <peapod2007@gmail.com>
2021-01-10 11:47:37 -0500

    Adds caching logic for 'all wheel' searchs
    Signed-off-by: Cole Gentry <peapod2007@gmail.com>

--- store/data.js
+++ store/data.js
@@ -1,11 +1,25 @@
 export const state = () => ({
   youtubeStats: [],
+  allWheels: {
+    currentSize: null,
+    wheels: {
+      10: [],
+      12: [],
+      13: []
+    }
+  },
   popperDismissed: false
 });
 
 export const mutations = {
   saveYoutube (state, stats) {
     state.youtubeStats = stats;
+  },
+  saveAllWheels (state, wheels) {
+    state.allWheels.wheels[state.allWheels.currentSize] = wheels;
+  },
+  setCurrentSize (state, currentSize) {
+    state.allWheels.currentSize = currentSize;
   },
   updatePopperStatus (state) {
     state.popperDismissed = true;
@@ -15,5 +29,8 @@ export const mutations = {
 export const getters = {
   getPopperStatus (state) {
     return state.popperDismissed;
+  },
+  getWheels (state) {
+    return state.allWheels;
   }
 };
