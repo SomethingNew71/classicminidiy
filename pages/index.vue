c106f8692588f879d67de3421278e06f9a1cfbd0
Cole Gentry <peapod2007@gmail.com>
2021-02-02 13:13:31 -0500

    Updates Highcharts and various other node packages
    Signed-off-by: Cole Gentry <peapod2007@gmail.com>

--- pages/index.vue
+++ pages/index.vue
@@ -19,7 +19,7 @@
               Keeping the Classics on the Road
             </h4>
             <p class="is-size-5  pt-20">
-              Classic Mini DIY started out of my small driveway workshop in 2015. I always focus on two things: keeping your Classic Mini on the road and making DIY car work accessible for all skill levels. I make DIY videos and tutorials showing exactly how to complete a wide range of jobs on your Classic Mini. I also partner with world-class manufacturers to deliver top-of-the-line products to personalize and ensure the performance of your Classic Mini.
+              Classic Mini DIY started out of my small driveway workshop in 2015. I always focus on two things: keeping your Classic Mini on the road and making DIY car work accessible for all skill levels. I make DIY videos and tutorials showing exactly how to complete a wide range of jobs on your Classic Mini. I also partner with world-class manufacturers to deliver top-of-the-line products to personalize and ensure the performance of your Classic Mini.
             </p>
           </div>
           <div class="column is-4">
