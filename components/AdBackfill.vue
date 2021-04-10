655eed580f8a74f8a5848fdcb3f9f8bc6178bb25
Cole Gentry <peapod2007@gmail.com>
2021-04-10 17:29:09 -0400

    Removes google ads for a while because it's serving gross ads
    Signed-off-by: Cole Gentry <peapod2007@gmail.com>

--- components/AdBackfill.vue
+++ components/AdBackfill.vue
@@ -1,38 +1,53 @@
 <template>
   <div>
     <template v-if="size === 'large'">
-      <h3 class="fancy-font-book-oblique">
-        <i class="fad fa-ad"></i> SUPPORT THE CHANNEL
-      </h3>
-      <p class="is-size-5 pt-5">
-        Classic Mini DIY is <strong>completely free resource</strong> supported by our viewers and ads. If you are interested in helping to keep the channel alive, consider supporting on Patreon or by disabling your adblocker while using the website.
-      </p>
-      <a class="mt-4 button is-medium is-patreon" rel="noopener" href="https://patreon.com/classicminidiy" target="_blank">
-        <span class="icon">
-          <i class="fab fa-patreon" />
-        </span>
-        <span>Contribute</span>
-      </a>
-      <a class="mt-4 button is-medium is-light" rel="noopener" href="https://help.getadblock.com/support/solutions/articles/6000055743-how-to-disable-adblock-on-specific-sites" target="_blank">
-        <span class="icon">
-          <i class="fad fa-ad" />
-        </span>
-        <span>Disable AdBlockers</span>
-      </a>
+      <div class="columns">
+        <div class="column is-3 avatar-container">
+          <b-image src="https://classicminidiy.s3.amazonaws.com/misc/avatar.png" alt="Image of my car on jack stands" />
+        </div>
+        <div class="column is-9">
+          <h3 class="fancy-font-book-oblique is-size-5">
+            <i class="fad fa-hands-heart"></i> SUPPORT THE SITE
+          </h3>
+          <p class="is-size-5 pt-5">
+            Classic Mini DIY is <strong>completely free resource</strong> supported by our viewers. If you are interested in helping to keep the channel alive, consider supporting on Patreon.
+          </p>
+          <p class="pt-3 ">
+            <strong>Membership comes with tons of free perks and benefits!</strong>
+          </p>
+          <p>
+            <a class="mt-4 button is-medium is-patreon" rel="noopener" href="https://patreon.com/classicminidiy" target="_blank">
+              <span class="icon">
+                <i class="fab fa-patreon" />
+              </span>
+              <span>Contribute Today</span>
+            </a>
+          </p>
+        </div>
+      </div>
     </template>
     <template v-if="size === 'small'">
-      <h3 v-if="size === 'small'" class="fancy-font-book-oblique is-size-6">
-        <i class="fad fa-ad"></i> CONSIDER DISABLING ADS
-      </h3>
-      <p v-if="size === 'small'" class="is-size-6 pt-5">
-        Classic Mini DIY is <strong>completely free resource</strong> supported by our viewers and ads. Please consider disabling your adblocker.
-      </p>
-      <a v-if="size === 'small'" class="mt-4 button is-medium is-primary" rel="noopener" href="https://patreon.com/classicminidiy" target="_blank">
-        <span class="icon">
-          <i class="fab fa-patreon" />
-        </span>
-        <span>Contribute</span>
-      </a>
+      <div class="columns is-multiline">
+        <div class="column is-12">
+          <h3 v-if="size === 'small'" class="fancy-font-book-oblique is-size-5 pt-2">
+            <i class="fad fa-hands-heart"></i> SUPPORT THE SITE
+          </h3>
+        </div>
+        <div class="column is-12">
+          <p v-if="size === 'small'" class="is-size-6">
+            Classic Mini DIY is <strong>completely free resource</strong> supported by our viewers. If you are interested in helping to keep the channel alive, consider supporting on Patreon.
+          </p>
+          <p class="pt-3 ">
+            <strong>Membership comes with tons of free perks and benefits!</strong>
+          </p>
+          <a v-if="size === 'small'" class="mt-4 button is-medium is-patreon" rel="noopener" href="https://patreon.com/classicminidiy" target="_blank">
+            <span class="icon">
+              <i class="fab fa-patreon" />
+            </span>
+            <span>Contribute Today</span>
+          </a>
+        </div>
+      </div>
     </template>
   </div>
 </template>
--- layouts/default.vue
+++ layouts/default.vue
@@ -5,7 +5,7 @@
   </div>
 </template>
 <script>
-import axios from 'axios';
+// import axios from 'axios';
 import patreonPopper from '~/components/PatreonPopper';
 
 export default {
@@ -17,15 +17,18 @@ export default {
   },
   methods: {
     attemptAdLoad (searchPayload) {
-      axios.get('/adblocker.js')
-        .then((response) => {
-          this.$store.commit('data/setAdStatus', true);
-          console.log('Ads served');
-        })
-        .catch((error) => {
-          this.$store.commit('data/setAdStatus', false);
-          console.log('Ads blocked', error);
-        });
+      // Comment this out to enable ads again
+      this.$store.commit('data/setAdStatus', false);
+      // Comment this back in to enable ads again
+      // axios.get('/adblocker.js')
+      //   .then((response) => {
+      //     this.$store.commit('data/setAdStatus', true);
+      //     console.log('Ads served');
+      //   })
+      //   .catch((error) => {
+      //     this.$store.commit('data/setAdStatus', false);
+      //     console.log('Ads blocked', error);
+      //   });
     }
   }
 };
