d62c5a1f1ad8aef7aa569697e6d22ae2f9a31aad
Cole Gentry <peapod2007@gmail.com>
2023-09-06 19:34:24 -0400

    Comments sitemap code back in
    Signed-off-by: Cole Gentry <peapod2007@gmail.com>

--- server/api/_sitemap-urls.ts
+++ server/api/_sitemap-urls.ts
@@ -2,24 +2,24 @@ import { ParsedContent } from '@nuxt/content/dist/runtime/types';
 import { DateTime } from 'luxon';
 
 //server/api/_sitemap-urls.ts
-// export default cachedEventHandler(
-//   async () => {
-//     const blogPages = await $fetch<ParsedContent>('/api/_content/query', {
-//       method: 'GET',
-//       query: {
-//         _params: { where: [{ _path: '--REGEX /^/' }], limit: 100 },
-//       },
-//     }).then((res) => res);
+export default cachedEventHandler(
+  async () => {
+    const blogPages = await $fetch<ParsedContent>('/api/_content/query', {
+      method: 'GET',
+      query: {
+        _params: { where: [{ _path: '--REGEX /^/' }], limit: 100 },
+      },
+    }).then((res) => res);
 
-//     return blogPages.map((page: any) => ({
-//       loc: page._path,
-//       lastmod: DateTime.fromFormat(page.date, 'LLLL dd yyyy'),
-//       changefreq: 'daily',
-//       priority: 0.8,
-//     }));
-//   },
-//   {
-//     name: 'Classic Mini DIY',
-//     maxAge: 60 * 10, // cache URLs for 10 minutes
-//   }
-// );
+    return blogPages.map((page: any) => ({
+      loc: page._path,
+      lastmod: DateTime.fromFormat(page.date, 'LLLL dd yyyy'),
+      changefreq: 'daily',
+      priority: 0.8,
+    }));
+  },
+  {
+    name: 'Classic Mini DIY',
+    maxAge: 60 * 10, // cache URLs for 10 minutes
+  }
+);
