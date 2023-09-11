354e0baff14494748426e807fcce95882c2e01f2
Cole Gentry <peapod2007@gmail.com>
2023-09-11 14:08:40 -0400

    Adds common clearances page resolving #346
    Signed-off-by: Cole Gentry <peapod2007@gmail.com>

---
+++ pages/technical/clearance.vue
@@ -0,0 +1,103 @@
+<template>
+  <div>
+    <hero :navigation="true" :title="'Common Clearances'" />
+    <section class="section">
+      <div class="columns is-multiline">
+        <div class="column is-12">
+          <nav class="breadcrumb" aria-label="breadcrumbs">
+            <ul>
+              <li>
+                <NuxtLink to="/">
+                  <span class="icon is-small">
+                    <i class="fad fa-home" aria-hidden="true" />
+                  </span>
+                  <span>Home</span>
+                </NuxtLink>
+              </li>
+              <li>
+                <NuxtLink to="/technical">
+                  <span class="icon is-small">
+                    <i class="fad fa-info-circle" aria-hidden="true" />
+                  </span>
+                  <span>Technical Info</span>
+                </NuxtLink>
+              </li>
+              <li class="is-active">
+                <NuxtLink to="">
+                  <span class="icon is-small">
+                    <i class="fad fa-ruler" aria-hidden="true" />
+                  </span>
+                  <span>Common Clearances</span>
+                </NuxtLink>
+              </li>
+            </ul>
+          </nav>
+        </div>
+        <div v-for="(table, name, index) in tables" :key="`${name}-${index}`" class="column is-12">
+          <o-collapse class="card" animation="slide">
+            <template #trigger="props">
+              <div class="card-header" role="button">
+                <h2 class="card-header-title">
+                  <i :class="table.icon"></i>
+                  <span class="pl-1">{{ table.title }}</span>
+                </h2>
+                <a class="card-header-icon">
+                  <o-icon pack="fas" :icon="props.open ? 'caret-up' : 'caret-down'"> </o-icon>
+                </a>
+              </div>
+            </template>
+            <div class="card-content">
+              <client-only>
+                <o-table
+                  :data="table.items"
+                  :columns="table.columns"
+                  :narrowed="true"
+                  :hoverable="true"
+                  :paginated="table.items.length >= 14 ? true : false"
+                  :per-page="10"
+                  icon-pack="fas"
+                  :mobile-cards="false"
+                />
+              </client-only>
+            </div>
+          </o-collapse>
+        </div>
+        <div class="column is-12">
+          <div class="card">
+            <div class="card-content">
+              <patreon-card size="large" />
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  </div>
+</template>
+
+<script lang="ts" setup>
+  const { data: tables } = await useFetch('/api/clearance');
+  useHead({
+    title: 'Tech - Mini Clearances',
+    meta: [
+      {
+        hid: 'description',
+        name: 'description',
+        content: 'Detailed torque specifications can be found online right here at Classic Mini DIY.',
+      },
+    ],
+  });
+  useSeoMeta({
+    ogTitle: 'Tech - Mini Clearances',
+    ogDescription:
+      'Detailed torque specifications for the Classic Mini can be found online right here at Classic Mini DIY.',
+    ogUrl: 'classicminidiy.com/technical/clearance',
+    ogImage: 'https://classicminidiy.s3.amazonaws.com/cloud-icon/icons8-blueprint-zoom-100.png',
+    ogType: 'website',
+  });
+</script>
+
+<style lang="scss" scoped>
+  .card-header {
+    background-color: whitesmoke;
+  }
+</style>
