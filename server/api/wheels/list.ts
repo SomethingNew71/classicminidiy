628dbfb0a5f951068f9f29bd9d46f06eac16354d
Cole Gentry <peapod2007@gmail.com>
2023-11-30 11:13:13 -0500

    Tries new image situation
    Signed-off-by: Cole Gentry <peapod2007@gmail.com>

--- server/api/wheels/list.ts
+++ server/api/wheels/list.ts
@@ -4,9 +4,6 @@ import { DynamoDBDocumentClient, ScanCommand } from '@aws-sdk/lib-dynamodb';
 export default defineEventHandler(async () => {
   let allWheels;
   const config = useRuntimeConfig();
-  // const body = await readBody(event);
-  // console.log(body);
-
   const docClient = DynamoDBDocumentClient.from(
     new DynamoDBClient({
       region: 'us-east-1',
@@ -27,25 +24,4 @@ export default defineEventHandler(async () => {
   }
 
   return allWheels;
-
-  // const getMembers = async (_key: any) => {
-  //   const scanCommand = new ScanCommand({
-  //     TableName: 'wheels',
-  //     Limit: 25,
-  //     ExclusiveStartKey: _key,
-  //   });
-  //   const results = await docClient.send(scanCommand);
-  //   allWheels.push(results.Items);
-  //   return results.LastEvaluatedKey;
-  // };
-  // try {
-  //   let key;
-  //   do {
-  //     key = await getMembers(key);
-  //   } while (typeof key !== 'undefined');
-
-  //   return allWheels;
-  // } catch (error: any) {
-  //   throw new Error(error);
-  // }
 });
