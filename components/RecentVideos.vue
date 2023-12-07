aef448d5cd0e40113ad7d091bc72faecf5e3e1fe
Cole Gentry <peapod2007@gmail.com>
2023-12-07 15:08:44 -0500

    Fixes pesky bug with youtube stats and vids
    Signed-off-by: Cole Gentry <peapod2007@gmail.com>

--- components/RecentVideos.vue
+++ components/RecentVideos.vue
@@ -1,88 +1,41 @@
 <template>
-  <div>
+  <v-row>
     <h3 class="fancy-font-bold is-size-3 has-text-centered pb-5">Recent Videos</h3>
 
-    <template v-if="isLoading">
-      <div class="columns is-multiline">
-        <div v-for="(item, index) in 2" :key="index" class="column is-half">
-          <div class="card">
-            <div class="card-image">
-              <o-skeleton height="100px"></o-skeleton>
-            </div>
-            <div class="card-content">
-              <div class="content">
-                <p class="title is-6">
-                  <o-skeleton></o-skeleton>
-                </p>
-                <p class="subtitle is-7 pt-2">
-                  <o-skeleton></o-skeleton>
-                </p>
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
+    <template v-if="pending">
+      <v-row>
+        <v-col cols="12" md="3" v-for="item in 3">
+          <v-skeleton-loader class="mx-auto border" max-width="300" type="image, article"></v-skeleton-loader>
+        </v-col>
+      </v-row>
     </template>
 
-    <div class="columns is-multiline">
-      <div class="column is-one-third-desktop" v-for="video in videos">
-        <div class="card">
-          <div class="card-image">
-            <figure class="image">
-              <a :href="video.videoUrl" target="_blank">
-                <picture :alt="video.title + ' thumbnail'">
-                  <source media="(max-width: 799px)" :srcset="video.thumbs.medium" />
-                  <source media="(max-width: 992px)" :srcset="video.thumbs.high" />
-                  <source media="(max-width: 1200px)" :srcset="video.thumbs.standard" />
-                  <img loading="lazy" :src="video.thumbs.maxres" :alt="'Thumbnail for ' + videos.title" />
-                </picture>
-              </a>
-            </figure>
-          </div>
-          <div class="card-content">
-            <div class="content">
-              <p class="title is-6">
-                {{ video.title }}
-              </p>
-              <p class="subtitle is-7 pt-2">published on {{ video.publishedOn }}</p>
-            </div>
-          </div>
-        </div>
-      </div>
-    </div>
-  </div>
+    <template v-else-if="error">
+      <p>Unable to load youtube data</p>
+    </template>
+
+    <v-row v-else-if="videos">
+      <v-col cols="12" md="4" v-for="video in videos">
+        <v-card class="mx-auto" max-width="344" :href="video.videoUrl">
+          <v-img :src="video.thumbnails.maxres" height="250px">
+            <template #sources>
+              <source media="(max-width: 799px)" :srcset="video.thumbnails.medium" />
+              <source media="(max-width: 992px)" :srcset="video.thumbnails.high" />
+              <source media="(max-width: 1200px)" :srcset="video.thumbnails.standard" />
+            </template>
+          </v-img>
+          <v-card-title>{{ video.title }}</v-card-title>
+          <v-card-subtitle class="pb-5"> published on {{ video.publishedOn }} </v-card-subtitle>
+        </v-card>
+      </v-col>
+    </v-row>
+  </v-row>
 </template>
 
 <script setup lang="ts">
-  let videos: any;
-  let isLoading = true;
-  await useFetch('/api/videos')
-    .then((response: any) => {
-      if (response?.data?._rawValue) {
-        videos = response.data._rawValue.map((video: any) => {
-          return {
-            title: video.title,
-            thumbs: video.thumbnails,
-            publishedOn: DateTime.fromISO(video.publishedOn).toFormat('LLL dd, yyyy'),
-            videoUrl: video.videoUrl,
-          };
-        });
-      } else {
-        videos = [];
-      }
-    })
-    .finally(() => {
-      isLoading = false;
-    });
+  import { VCol, VRow } from 'vuetify/components/VGrid';
+  import { VCard, VCardTitle, VCardSubtitle } from 'vuetify/components/VCard';
+  import { VImg } from 'vuetify/components/VImg';
+  import { VSkeletonLoader } from 'vuetify/components/VSkeletonLoader';
+  const { data: videos, pending, error } = await useFetch('/api/videos');
 </script>
-
-<script lang="ts">
-  import { DateTime } from 'luxon';
-  export default defineComponent({});
-</script>
-
-<style lang="scss" scoped>
-  .has-shadow {
-    box-shadow: none;
-  }
-</style>
--- components/Stats.vue
+++ components/Stats.vue
@@ -4,25 +4,34 @@
       <div class="level-item has-text-centered">
         <div>
           <p class="heading">Minutes Watched</p>
-          <p v-if="stats.views" class="title">
+          <p v-if="pending"></p>
+          <p v-else-if="error">??</p>
+          <p v-else-if="stats && stats.views" class="title">
             {{ stats.views }}
           </p>
+          <p v-else>??</p>
         </div>
       </div>
       <div class="level-item has-text-centered">
         <div>
           <p class="heading">Subscribers</p>
-          <p v-if="stats.subscribers" class="title">
+          <p v-if="pending"></p>
+          <p v-else-if="error">??</p>
+          <p v-else-if="stats && stats.subscribers" class="title">
             {{ stats.subscribers }}
           </p>
+          <p v-else>??</p>
         </div>
       </div>
       <div class="level-item has-text-centered">
         <div>
           <p class="heading">videos</p>
-          <p v-if="stats.videos" class="title">
+          <p v-if="pending"></p>
+          <p v-else-if="error">??</p>
+          <p v-else-if="stats && stats.videos" class="title">
             {{ stats.videos }}
           </p>
+          <p v-else>??</p>
         </div>
       </div>
     </nav>
@@ -30,22 +39,13 @@
 </template>
 
 <script lang="ts" setup>
-  let stats: any;
-  await useFetch('/api/stats')
-    .then((response: any) => {
-      stats = { ...response.data._rawValue };
-    })
-    .catch(() => {
-      stats = {
-        views: undefined,
-        subscribers: undefined,
-        videos: undefined,
-      };
-    });
+  const { data: stats, pending, error } = await useFetch('/api/stats');
 </script>
 
 <style lang="scss" scoped>
   .card {
-    box-shadow: 0 0.5em 1em -0.125em rgba(10, 10, 10, 0.4), 0 0px 0 1px rgba(10, 10, 10, 0.02);
+    box-shadow:
+      0 0.5em 1em -0.125em rgba(10, 10, 10, 0.4),
+      0 0px 0 1px rgba(10, 10, 10, 0.02);
   }
 </style>
