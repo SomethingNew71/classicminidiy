aaaec31cdfb579f0c7a2dfe7b7ac8f1769eb13e3
Cole Gentry <peapod2007@gmail.com>
2024-01-05 09:41:50 -0500

    Changes needle tables to vuetify
    Signed-off-by: Cole Gentry <peapod2007@gmail.com>

--- server/api/needles.ts
+++
@@ -1,9 +0,0 @@
-import needles from '../../data/needles.json';
-import starterNeedles from '../../data/default-needles.json';
-
-export default defineEventHandler(() => {
-  return {
-    needles,
-    starterNeedles,
-  };
-});
