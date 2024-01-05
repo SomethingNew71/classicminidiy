94557d9dde1ad1a6a07f889ef75ff7d17feabf53
Cole Gentry <peapod2007@gmail.com>
2024-01-05 10:07:57 -0500

    Removes oruga from maps page
    Signed-off-by: Cole Gentry <peapod2007@gmail.com>

--- server/api/github/commits.ts
+++ server/api/github/commits.ts
@@ -1,5 +1,6 @@
 import { request } from '@octokit/request';
 import * as _ from 'lodash';
+import { DateTime } from 'luxon';
 
 export default defineEventHandler(async (event) => {
   const config = useRuntimeConfig();
@@ -10,7 +11,15 @@ export default defineEventHandler(async (event) => {
       },
       owner: 'SomethingNew71',
       repo: 'MiniECUMaps',
-    }).then(({ data }) => data);
+    }).then(({ data }) => {
+      return data.map((item) => {
+        const date = item?.commit?.committer?.date;
+        return {
+          ...item,
+          date: date ? DateTime.fromISO(date).toFormat('LLL dd') : 'Missing',
+        };
+      });
+    });
   } catch (error) {
     throw new Error(`Error getting github releases - ${error}`);
   }
