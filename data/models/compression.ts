8dc4acbcfa265264f8285830ea0a69ca7f815755
Cole Gentry <peapod2007@gmail.com>
2024-01-19 17:11:38 -0500

    removes oruga from compression calculator
    Signed-off-by: Cole Gentry <peapod2007@gmail.com>

---
+++ data/models/compression.ts
@@ -0,0 +1,198 @@
+export const formOptions = {
+  pistonOptions: [
+    {
+      label: '62.9mm',
+      subtitle: '850',
+      value: 6.29,
+    },
+    {
+      label: '- 64.58mm',
+      subtitle: '998/1098',
+      value: 6.458,
+    },
+    {
+      label: '+20 - 65.08mm',
+      subtitle: '998/1098',
+      value: 6.5088,
+    },
+    {
+      label: '+40 - 65.59mm',
+      subtitle: '998/1098',
+      value: 6.5596,
+    },
+    {
+      label: '+60 - 66.10mm',
+      subtitle: '998/1098',
+      value: 6.6104,
+    },
+    {
+      label: '+80 - 66.61mm',
+      subtitle: '998/1098',
+      value: 6.6612,
+    },
+    {
+      label: 'Stock - 70.60mm',
+      subtitle: '1275',
+      value: 7.06,
+    },
+    {
+      label: '+20 - 71.10mm',
+      subtitle: '1275',
+      value: 7.1108,
+    },
+    {
+      label: '+40 - 71.61mm',
+      subtitle: '1275',
+      value: 7.1616,
+    },
+    {
+      label: '+60 - 72.12mm',
+      subtitle: '1275',
+      value: 7.2124,
+    },
+    {
+      label: '+80 - 72.62mm',
+      subtitle: '1275',
+      value: 7.262,
+    },
+    {
+      label: '1360cc | 73mm',
+      subtitle: '1275',
+      value: 7.3,
+    },
+    {
+      label: '1380cc | 73.5mm',
+      subtitle: '1275',
+      value: 7.35,
+    },
+    {
+      label: '1400cc | 74mm',
+      subtitle: '1275',
+      value: 7.4,
+    },
+    {
+      label: 'Russel Engineering | 72.5mm',
+      subtitle: '1275',
+      value: 7.25,
+    },
+  ],
+  crankshaftOptions: [
+    {
+      label: '850cc - 68.26mm',
+      subtitle: 'Standard',
+      value: 6.826,
+    },
+    {
+      label: '998cc - 76.2mm',
+      subtitle: 'Standard',
+      value: 7.62,
+    },
+    {
+      label: '1100cc - 83.72mm',
+      subtitle: 'Standard',
+      value: 8.372,
+    },
+    {
+      label: '1275cc - 81.28mm',
+      subtitle: 'Standard',
+      value: 8.128,
+    },
+    {
+      label: 'South African 1275cc - 70.2mm',
+      subtitle: 'Special',
+      value: '7.02',
+    },
+    {
+      label: '61.91mm',
+      subtitle: 'Stroked',
+      value: '6.191',
+    },
+    {
+      label: '68.26mm',
+      subtitle: 'Stroked',
+      value: '6.826',
+    },
+    {
+      label: '76mm',
+      subtitle: 'Stroked',
+      value: '7.6',
+    },
+    {
+      label: '84mm',
+      subtitle: 'Stroked',
+      value: '8.4',
+    },
+    {
+      label: '86mm',
+      subtitle: 'Stroked',
+      value: '8.6',
+    },
+  ],
+  headGasketOptions: [
+    {
+      label: '850 - 2.4cc',
+      value: 2.4,
+    },
+    {
+      label: '998 - 2.8cc',
+      value: 2.8,
+    },
+    {
+      label: '1275 - 3.4cc',
+      value: 3.4,
+    },
+    {
+      label: 'MED Cometic 1275 - 3.01cc',
+      value: 3.01,
+    },
+    {
+      label: 'MED Cometic 1380 - 3.15cc',
+      value: 3.15,
+    },
+    {
+      label: 'Specialist Components GEG300 - 3.4cc',
+      value: 3.4001,
+    },
+    {
+      label: 'Custom Volume',
+      value: 0,
+    },
+  ],
+  decompPlateOptions: [
+    {
+      label: 'None (0cc)',
+      subtitle: '',
+      value: 0,
+    },
+    {
+      label: '3cc + Extra Head Gasket (5.8cc)',
+      subtitle: '998cc Plates',
+      value: 5.8,
+    },
+    {
+      label: '6cc + Extra Head Gasket (8.8cc)',
+      subtitle: '998cc Plates',
+      value: 8.8,
+    },
+    {
+      label: '9cc + Extra Head Gasket (11.8cc)',
+      subtitle: '998cc Plates',
+      value: 11.8,
+    },
+    {
+      label: '3cc + Extra Head Gasket (6.4cc)',
+      subtitle: '1275cc Plates',
+      value: 6.4,
+    },
+    {
+      label: '3cc + Extra Head Gasket 9.4cc)',
+      subtitle: '1275cc Plates',
+      value: 9.4,
+    },
+    {
+      label: '3cc + Extra Head Gasket (12.4cc)',
+      subtitle: '1275cc Plates',
+      value: 12.4,
+    },
+  ],
+};
