42934a62955ebacac79302830620b9196a922de1
Cole Gentry <peapod2007@gmail.com>
2024-10-21 15:35:53 -0400

    converts back to using Github
    Signed-off-by: Cole Gentry <peapod2007@gmail.com>

--- data/models/github.ts
+++ data/models/github.ts
@@ -1,4 +1,28 @@
-export interface IGithubParsedResponse {
-  latestRelease: string;
-  releases: any;
+export interface IGithubReleaseParsedResponse {
+  latestRelease: string | null;
+  releases: ReleaseItem[];
+}
+
+export interface GithubRawResponseRelease {
+  data: ReleaseItem[];
+}
+export interface ReleaseItem {
+  url: string | null;
+  assets_url: string | null;
+  upload_url: string | null;
+  html_url: string | null;
+  id: number | null;
+  author: any;
+  node_id: string | null;
+  tag_name: string | null;
+  target_commitish: string | null;
+  name: string | null;
+  draft: boolean;
+  prerelease: boolean;
+  created_at: string | null;
+  published_at: string | null;
+  assets: any;
+  tarball_url: string | null;
+  zipball_url: string | null;
+  body?: string | null | undefined;
 }
--- server/api/github/releases.ts
+++ server/api/github/releases.ts
@@ -1,22 +1,21 @@
 import { request } from '@octokit/request';
-import type { IGithubParsedResponse } from '~/data/models/github';
+import type { GithubRawResponseRelease, IGithubReleaseParsedResponse, ReleaseItem } from '~/data/models/github';
 
-export default defineEventHandler(async (event) => {
+export default defineEventHandler(async (): Promise<IGithubReleaseParsedResponse> => {
   const config = useRuntimeConfig();
 
   try {
     return await request('GET /repos/{owner}/{repo}/releases', {
-      headers: {
-        authorization: config.app.githubAPIKey,
-      },
+      headers: { authorization: config.app.githubAPIKey },
       owner: 'SomethingNew71',
       repo: 'MiniECUMaps',
-    }).then(
-      (response): IGithubParsedResponse => ({
-        latestRelease: response.data[0].tag_name,
-        releases: response.data,
-      })
-    );
+    }).then(({ data }): IGithubReleaseParsedResponse => {
+      const parsed: IGithubReleaseParsedResponse = {
+        latestRelease: data[0].tag_name || 'No releases',
+        releases: data,
+      };
+      return parsed;
+    });
   } catch (error) {
     throw new Error(`Error getting github releases - ${error}`);
   }
