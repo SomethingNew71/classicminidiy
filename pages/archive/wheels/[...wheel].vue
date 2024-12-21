6abe528e3892a14116b63e3551c5d70576ef1395
Cole Gentry <peapod2007@gmail.com>
2024-12-20 21:32:49 -0500

    Reworks wheel library so its more usable
    Signed-off-by: Cole Gentry <peapod2007@gmail.com>

--- pages/archive/wheels/[...wheel].vue
+++ pages/archive/wheels/[...wheel].vue
@@ -1,6 +1,7 @@
 <script lang="ts" setup>
   import { SpeedInsights } from '@vercel/speed-insights/nuxt';
   import { HERO_TYPES, SocialShareData } from '~/data/models/generic';
+  import { shareWheelItem } from '~/data/models/helper-utils';
   import type { IWheelsData } from '~/data/models/wheels';
   const { params } = await useRoute();
   const wheelId = ref(params.wheel);
@@ -59,70 +60,76 @@
       <v-col cols="12" md="10" offset-md="1">
         <v-card :loading="status === 'pending'" elevation="4">
           <template v-if="wheel">
-            <v-card-text class="py-0">
+            <v-card-text class="pl-10 py-0 pt-3">
               <v-row align="center" no-gutters>
-                <v-col cols="6">
-                  <h2 class="text-h5">{{ wheel.size }}</h2>
-                  <p class="text-capitalize pb-5">
-                    <v-icon icon="fad fa-ruler" size="18" color="primary" class="me-1 pb-1"></v-icon>
-                    {{ wheel.size }} x {{ wheel.width }}
-                  </p>
-                  <h3 class="text-h2">{{ wheel.name }}</h3>
+                <v-col cols="12" md="6">
+                  <h1 class="text-h4">{{ wheel.name }}</h1>
+                  <h2 class="text-h6 pt-4">{{ wheel.notes }}</h2>
                 </v-col>
 
-                <v-col cols="6" class="pt-2">
-                  <WheelImages :images="wheel.images" />
+                <v-col cols="12" md="6" class="pt-2">
+                  <v-carousel v-if="wheel.images && wheel.images.length > 1" height="300" hide-delimiters>
+                    <template v-for="image in wheel.images">
+                      <v-carousel-item :src="image.src"></v-carousel-item>
+                    </template>
+                  </v-carousel>
+                  <v-img
+                    :alt="`Image of the ${wheel.name} wheels`"
+                    max-height="300"
+                    max-width="300"
+                    v-else-if="wheel.images"
+                    cover
+                    :src="wheel.images[0].src"
+                  ></v-img>
                 </v-col>
               </v-row>
             </v-card-text>
 
-            <!-- <v-row class="d-flex py-3 justify-space-between">
+            <v-divider></v-divider>
+            <v-row class="d-flex py-3 justify-space-between">
               <v-col cols="12" sm="6" md="3">
-                <v-list-item density="compact" prepend-icon="fad fa-calendar">
-                  <v-list-item-title>Years</v-list-item-title>
-                  <v-list-item-subtitle v-if="color.pretty.Years">{{ color.pretty.Years }}</v-list-item-subtitle>
+                <v-list-item density="compact" prepend-icon="fad fa-arrow-right-to-line">
+                  <v-list-item-title>Offset</v-list-item-title>
+                  <v-list-item-subtitle v-if="wheel.offset">{{ wheel.offset }}</v-list-item-subtitle>
                   <v-list-item-subtitle v-else class="text-red-darken-2">Missing</v-list-item-subtitle>
                 </v-list-item>
               </v-col>
+              <v-divider vertical></v-divider>
               <v-col cols="12" sm="6" md="3">
-                <v-list-item density="compact" prepend-icon="fad fa-code-simple">
-                  <v-list-item-title>Short Code</v-list-item-title>
-                  <v-list-item-subtitle v-if="color.pretty['Short Code']">{{
-                    color.pretty['Short Code']
-                  }}</v-list-item-subtitle>
+                <v-list-item density="compact" prepend-icon="fad fa-arrows-to-line">
+                  <v-list-item-title>Diameter</v-list-item-title>
+                  <v-list-item-subtitle v-if="wheel.size">{{ wheel.size }}</v-list-item-subtitle>
                   <v-list-item-subtitle v-else class="text-red-darken-2">Missing</v-list-item-subtitle>
                 </v-list-item>
               </v-col>
+              <v-divider vertical></v-divider>
               <v-col cols="12" sm="6" md="3">
-                <v-list-item density="compact" prepend-icon="fad fa-square-code">
-                  <v-list-item-title>Ditzler PPG Code</v-list-item-title>
-                  <v-list-item-subtitle v-if="color.pretty['Ditzler PPG Code']">{{
-                    color.pretty['Ditzler PPG Code']
-                  }}</v-list-item-subtitle>
+                <v-list-item density="compact" prepend-icon="fad fa-arrows-left-right-to-line">
+                  <v-list-item-title>Width</v-list-item-title>
+                  <v-list-item-subtitle v-if="wheel.width">{{ wheel.width }}</v-list-item-subtitle>
                   <v-list-item-subtitle v-else class="text-red-darken-2">Missing</v-list-item-subtitle>
                 </v-list-item>
               </v-col>
-              <v-col cols="12" sm="6" md="3">
-                <v-list-item density="compact" prepend-icon="fad fa-square-code">
-                  <v-list-item-title>Dulux Code</v-list-item-title>
-                  <v-list-item-subtitle v-if="color.pretty['Dulux Code']">{{
-                    color.pretty['Dulux Code']
-                  }}</v-list-item-subtitle>
-                  <v-list-item-subtitle v-else class="text-red-darken-2">Missing</v-list-item-subtitle>
-                </v-list-item>
-              </v-col>
-            </v-row> -->
+            </v-row>
             <v-divider></v-divider>
             <v-card-actions class="d-flex justify-space-around">
-              <v-btn v-if="copied" prepend-icon="fad fa-link" disabled>Copied</v-btn>
-              <v-btn v-else prepend-icon="fad fa-link" @click="copyUrl()">Copy Link</v-btn>
-              <!-- <v-btn
-                color="brandGreen"
-                :href="`mailto:?subject=Mini Color Swatch - ${color.pretty.Name} | ${color.pretty.Code}&body=Color%20Name%20-%20${color.pretty.Name}%20%0ABMC%20Code%20-%20${color.pretty.Code}%20%0ADulux%20Code%20-%20${color.pretty['Dulux Code']}%20%0ADitzler%20Code%20-%20${color.pretty['Ditzler PPG Code']}%20`"
-                prepend-icon="fad fa-paper-plane"
-                >Email</v-btn
-              > -->
-              <v-btn color="primary" prepend-icon="fab fa-facebook" :href="socialShareUrl">Share</v-btn>
+              <v-btn v-if="copied" prepend-icon="fad fa-link" disabled size="large">Copied</v-btn>
+              <v-btn v-else prepend-icon="fad fa-link" @click="copyUrl()" size="large">Copy Link</v-btn>
+              <v-btn
+                @click="shareWheelItem(wheel.name, wheel.uuid)"
+                size="large"
+                prepend-icon="fad fa-solid fa-arrow-up-from-bracket"
+              >
+                Share
+              </v-btn>
+              <v-btn
+                class="text-center"
+                size="large"
+                prepend-icon="fad fa-edit"
+                :to="`/archive/wheels/submit?uuid=${wheel.uuid}`"
+              >
+                Contribute
+              </v-btn>
             </v-card-actions>
           </template>
         </v-card>
