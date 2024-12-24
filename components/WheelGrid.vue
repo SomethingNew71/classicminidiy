e39f3dbd24ebe51977da8ee01d0e21101561c352
Cole Gentry <peapod2007@gmail.com>
2024-12-23 20:57:18 -0500

    Adds materials and offsets filters to the wheels page
    Signed-off-by: Cole Gentry <peapod2007@gmail.com>

--- components/WheelGrid.vue
+++ components/WheelGrid.vue
@@ -3,35 +3,53 @@
   const search = ref('');
   const size = ref('list');
   const { data: wheels, status } = await useFetch<IWheelsData[]>(() => `/api/wheels/${size.value}`);
-  // const allOffsets = ref<string[]>([]);
-  // const selectedOffsets = ref<string[]>([]);
-  // const allMaterials = ref<string[]>([]);
-  // const selectedMaterials = ref<string[]>([]);
+  const filteredWheels = computed(() => {
+    if (!wheels.value) return [];
+    return wheels.value.filter((wheel) => {
+      const searchMatch = wheel.name.toLowerCase().includes(search.value.toLowerCase());
+      const offsetMatch = selectedOffsets.value.length === 0 || selectedOffsets.value.includes(wheel.offset.trim());
+      const materialMatch = selectedMaterials.value.length === 0 || selectedMaterials.value.includes(wheel.type.trim());
+      return searchMatch && offsetMatch && materialMatch;
+    });
+  });
+  const allOffsets = ref<string[]>([]);
+  const selectedOffsets = ref<string[]>([]);
+  const allMaterials = ref<string[]>([]);
+  const selectedMaterials = ref<string[]>([]);
+  const filtersActive = computed(
+    () => selectedOffsets.value.length > 0 || selectedMaterials.value.length > 0 || search.value !== ''
+  );
 
-  // function getAllOffsets() {
-  //   if (!wheels.value) return;
-  //   const offsets = wheels.value.map((wheel) => wheel.offset.trim());
-  //   allOffsets.value = Array.from(new Set(offsets))
-  //     .sort()
-  //     .filter((offset) => offset !== '');
-  // }
-  // function getAllMaterials() {
-  //   if (!wheels.value) return;
-  //   const materials = wheels.value.map((wheel) => wheel.type.trim());
-  //   allMaterials.value = Array.from(new Set(materials)).sort();
-  // }
-  // // Watch for changes in wheels data and update offsets
-  // watch(wheels, () => {
-  //   getAllOffsets();
-  //   getAllMaterials();
-  // });
+  function getAllOffsets() {
+    if (!wheels.value) return;
+    const offsets = wheels.value.map((wheel) => wheel.offset.trim());
+    allOffsets.value = Array.from(new Set(offsets))
+      .sort()
+      .filter((offset) => offset !== '');
+  }
+  function getAllMaterials() {
+    if (!wheels.value) return;
+    const materials = wheels.value.map((wheel) => wheel.type.trim());
+    allMaterials.value = Array.from(new Set(materials)).sort();
+  }
 
-  // await getAllOffsets();
-  // await getAllMaterials();
+  function clearFilters() {
+    search.value = '';
+    selectedOffsets.value = [];
+    selectedMaterials.value = [];
+  }
+  // Watch for changes in wheels data and update offsets
+  watch(wheels, () => {
+    getAllOffsets();
+    getAllMaterials();
+  });
+
+  await getAllOffsets();
+  await getAllMaterials();
 </script>
 
 <template>
-  <v-data-iterator :loading="status === 'pending'" :items="wheels || []" :items-per-page="12" :search="search">
+  <v-data-iterator :loading="status === 'pending'" :items="filteredWheels || []" :items-per-page="12" :search="search">
     <template v-slot:header>
       <v-card-title class="d-flex align-center pe-2">
         <v-icon icon="fad fa-tire fa-spin" class="me-1 py-2"></v-icon> &nbsp; Find Wheels
@@ -39,7 +57,7 @@
       </v-card-title>
       <v-card-text class="pb-10">
         <v-row>
-          <v-col cols="12" md="3">
+          <v-col cols="12" md="6">
             <v-btn-toggle v-model="size" mandatory rounded="10" color="primary" group elevation="2">
               <v-btn value="list" :active="size === 'list'"> All </v-btn>
               <v-btn value="ten" :active="size === 'ten'"> 10 </v-btn>
@@ -47,42 +65,49 @@
               <v-btn value="thirteen" :active="size === 'thirteen'"> 13 </v-btn>
             </v-btn-toggle>
           </v-col>
-          <v-col cols="12" md="9">
+          <v-col cols="12" md="6" class="d-flex justify-end">
+            <v-btn
+              :disabled="!filtersActive"
+              :variant="'flat'"
+              @click="clearFilters"
+              class="text-caption"
+              color="primary"
+              >Clear Filters</v-btn
+            >
+          </v-col>
+          <v-col cols="12">
             <v-text-field
               v-model="search"
               prepend-inner-icon="fad fa-search"
-              density="compact"
               placeholder="Search for anything (ex. Momos, 10in, ET20)"
-              single-line
-              flat
               hide-details
               variant="solo-filled"
             ></v-text-field>
           </v-col>
-          <!-- <v-col cols="12" md="6">
-              <v-select v-model="selectedOffsets" :items="allOffsets" label="Offsets" multiple>
-                <template v-slot:selection="{ item, index }">
-                  <v-chip v-if="index < 2">
-                    <span>{{ item.title }}</span>
-                  </v-chip>
-                  <span v-if="index === 2" class="text-grey text-caption align-self-center">
-                    (+{{ selectedOffsets.length - 2 }} others)
-                  </span>
-                </template>
-              </v-select>
-            </v-col>
-            <v-col cols="12" md="6">
-              <v-select v-model="selectedMaterials" :items="allMaterials" label="Material" multiple>
-                <template v-slot:selection="{ item, index }">
-                  <v-chip v-if="index < 2">
-                    <span>{{ item.title }}</span>
-                  </v-chip>
-                  <span v-if="index === 2" class="text-grey text-caption align-self-center">
-                    (+{{ selectedMaterials.length - 2 }} others)
-                  </span>
-                </template>
-              </v-select>
-            </v-col> -->
+          <v-col cols="12" md="6">
+            <v-select v-model="selectedOffsets" :items="allOffsets" label="Offsets" multiple>
+              <template v-slot:selection="{ item, index }">
+                <v-chip v-if="index < 2">
+                  <span>{{ item.title }}</span>
+                </v-chip>
+                <span v-if="index === 2" class="text-grey text-caption align-self-center">
+                  (+{{ selectedOffsets.length - 2 }} others)
+                </span>
+              </template>
+            </v-select>
+          </v-col>
+          <v-col cols="12" md="6">
+            <v-select v-model="selectedMaterials" :items="allMaterials" label="Material" multiple>
+              <template v-slot:selection="{ item, index }">
+                <v-chip v-if="index < 2">
+                  <span>{{ item.title }}</span>
+                </v-chip>
+                <span v-if="index === 2" class="text-grey text-caption align-self-center">
+                  (+{{ selectedMaterials.length - 2 }} others)
+                </span>
+              </template>
+            </v-select>
+          </v-col>
         </v-row>
       </v-card-text>
     </template>
@@ -90,12 +115,12 @@
     <template v-slot:default="{ items }">
       <v-row class="d-flex justify-center px-2">
         <v-col v-for="{ raw: wheel } in items" :key="wheel.uuid" cols="12" sm="4" md="3">
-          <v-card flat nuxt-link :to="`/archive/wheels/${wheel.uuid}`">
+          <v-card class="wheel-container" flat nuxt-link :to="`/archive/wheels/${wheel.uuid}`">
             <template v-if="wheel.images">
               <v-img
                 alt=""
-                class="mx-auto rounded-xl align-end"
-                gradient="to bottom, rgba(0,0,0,.1), rgba(0,0,0,.5)"
+                class="mx-auto rounded-xl align-end wheel-image"
+                gradient="to bottom, rgba(0,0,0,0), rgba(0,0,0,0.7)"
                 aspect-ratio="1"
                 cover
                 :src="
@@ -105,7 +130,7 @@
                     : wheel.images[0].src
                 "
               >
-                <span class="float-right pr-2">
+                <span class="float-right wheel-chip pr-2">
                   <v-chip v-if="wheel.size === '10'" color="green" class="" variant="flat"> {{ wheel.size }}in </v-chip>
                   <v-chip v-if="wheel.size === '12'" color="orange" variant="flat"> {{ wheel.size }}in </v-chip>
                   <v-chip v-if="wheel.size === '13'" color="blue" variant="flat"> {{ wheel.size }}in </v-chip>
@@ -121,6 +146,11 @@
     </template>
 
     <template v-slot:footer="{ page, pageCount, prevPage, nextPage }">
+      <v-col v-if="filteredWheels.length === 0" cols="12" class="text-center">
+        <v-card flat>
+          <v-card-text class="text-body">No wheels meeting current filters exist</v-card-text>
+        </v-card>
+      </v-col>
       <div class="d-flex align-center justify-center pa-4">
         <v-btn
           :disabled="page === 1"
@@ -151,3 +181,13 @@
     </template>
   </v-data-iterator>
 </template>
+
+<style scoped>
+  .wheel-chip {
+    position: absolute;
+    top: 0;
+    right: 0;
+    margin-top: 0.5rem;
+    margin-right: 0.3rem;
+  }
+</style>
