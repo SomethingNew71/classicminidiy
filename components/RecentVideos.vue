43ee9ff79943bbb7314b6ddbebd3763cb308e824
Cole Gentry <peapod2007@gmail.com>
2025-05-21 16:07:21 -0400

    Finishes migrating homepage
    Signed-off-by: Cole Gentry <peapod2007@gmail.com>

--- components/RecentVideos.vue
+++ components/RecentVideos.vue
@@ -3,30 +3,31 @@
 </script>
 
 <template>
-  <v-col cols="12">
-    <h3 class="fancy-font-bold is-size-3 has-text-centered pb-5">Recent Videos</h3>
-  </v-col>
+  <div class="col-span-12">
+    <h3 class="font-bold text-3xl text-center pb-5">Recent Videos</h3>
+  </div>
 
-  <v-col v-if="status === 'pending'" cols="12" md="3" v-for="item in 3">
-    <v-skeleton-loader class="mx-auto border" max-width="300" type="image, article"></v-skeleton-loader>
-  </v-col>
-  <v-col v-else-if="error" cols="12" md="4" v-for="item in 3">
-    <v-skeleton-loader class="mx-auto border" max-width="300" type="image, article"></v-skeleton-loader>
-  </v-col>
+  <div v-if="status === 'pending'" class="col-span-12 md:col-span-3" v-for="item in 3">
+    <!-- <v-skeleton-loader class="mx-auto border" max-width="300" type="image, article"></v-skeleton-loader> -->
+  </div>
+  <div v-else-if="error" class="col-span-12">
+    <p>Unable to load youtube data</p>
+  </div>
 
-  <v-col v-else-if="videos" cols="12" md="4" v-for="video in videos">
-    <v-card class="mx-auto" max-width="344" :href="video.videoUrl">
-      <v-img alt="Thumbnail image for youtube video" :src="video.thumbnails.maxres" height="250px">
-        <template #sources>
-          <source media="(max-width: 799px)" :srcset="video.thumbnails.medium" />
-          <source media="(max-width: 992px)" :srcset="video.thumbnails.high" />
-          <source media="(max-width: 1200px)" :srcset="video.thumbnails.standard" />
-        </template>
-      </v-img>
-      <v-card-title>{{ video.title }}</v-card-title>
-      <v-card-subtitle class="pb-5"> published on {{ video.publishedOn }} </v-card-subtitle>
-    </v-card>
-  </v-col>
+  <template v-else-if="videos">
+    <div v-for="video in videos" class="card bg-base-100 col-span-12 md:col-span-4 shadow-xl">
+      <figure>
+        <nuxt-picture :src="video.thumbnails.maxres" alt="Video Thumbnail" />
+      </figure>
+      <div class="card-body">
+        <h2 class="card-title">{{ video.title }}</h2>
+        <p>Published on {{ video.publishedOn }}</p>
+        <div class="card-actions justify-end">
+          <a :href="video.videoUrl" class="btn btn-primary">Watch</a>
+        </div>
+      </div>
+    </div>
+  </template>
 </template>
 
 <style lang="scss">
