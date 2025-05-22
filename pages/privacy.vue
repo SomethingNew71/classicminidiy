7058977073a501ac1788474b4340cc419ba1c1cf
Cole Gentry <peapod2007@gmail.com>
2025-05-22 11:00:02 -0400

    Migrates the electrical and privacy pages
    Signed-off-by: Cole Gentry <peapod2007@gmail.com>

--- pages/privacy.vue
+++ pages/privacy.vue
@@ -68,46 +68,54 @@
   <hero
     title="Privacy Policy"
     :subtitle="'Classic Mini DIY'"
-    :size="'is-medium'"
+    :textSize="'3xl'"
     :special="true"
     :heroType="HERO_TYPES.HOME"
     :background="'/backdrop2'"
     :navigation="true"
   />
-  <v-container>
-    <v-row>
-      <v-col>
-        <h1 class="text-h4 font-weight-medium">Privacy Policy</h1>
+  <div class="container mx-auto px-4 py-8">
+    <div class="grid grid-cols-12 gap-4">
+      <div class="col-span-12">
+        <h1 class="text-3xl font-medium">Privacy Policy</h1>
 
-        <ol class="pl-5 pt-5">
+        <ol class="list-decimal pl-5 pt-5">
           <li>
-            <h3 class="text-h5">Acceptance of Terms</h3>
-            <p class="pt-2 pb-4">
+            <h3 class="text-xl font-medium mb-2">Acceptance of Terms</h3>
+            <p class="py-2 mb-4">
               By using our API client, you agree to be bound by the
-              <a class="pr-3" href="https://www.youtube.com/t/terms" target="_blank" rel="noopener noreferrer"
+              <a
+                class="link link-primary mr-3"
+                href="https://www.youtube.com/t/terms"
+                target="_blank"
+                rel="noopener noreferrer"
                 >Youtube Privacy Policy</a
               >.
             </p>
           </li>
           <li>
-            <h3 class="text-h5">Accessibility</h3>
-            <p class="pt-2 pb-4">
+            <h3 class="text-xl font-medium mb-2">Accessibility</h3>
+            <p class="py-2 mb-4">
               This Privacy Policy is easily accessible to all users via
-              <a href="http://www.google.com/policies/privacy" target="_blank" rel="noopener noreferrer"
+              <a
+                class="link link-primary"
+                href="http://www.google.com/policies/privacy"
+                target="_blank"
+                rel="noopener noreferrer"
                 >Google Privacy Policy</a
               >.
             </p>
           </li>
           <li>
-            <h3 class="text-h5">Use of YouTube API Services</h3>
-            <p class="pt-2 pb-4">
+            <h3 class="text-xl font-medium mb-2">Use of YouTube API Services</h3>
+            <p class="py-2 mb-4">
               Our API client uses YouTube API Services. By using our API client, you agree to the collection, storage,
               and usage of your data as described in this policy.
             </p>
           </li>
           <li>
-            <h3 class="text-h5">Data Collection, Storage, and Use</h3>
-            <p class="pt-2 pb-4">
+            <h3 class="text-xl font-medium mb-2">Data Collection, Storage, and Use</h3>
+            <p class="py-2 mb-4">
               We collect data directly from your device to provide and improve our services. This includes information
               about your device, IP address, and public cookie information. We do not collect any additional personal
               data for our use. This data is stored securely and used to enhance the user experience and ensure the
@@ -116,29 +124,31 @@
             </p>
           </li>
           <li>
-            <h3 class="text-h5">Data Processing and Sharing</h3>
-            <p class="pt-2 pb-4">
+            <h3 class="text-xl font-medium mb-2">Data Processing and Sharing</h3>
+            <p class="py-2 mb-4">
               We process your data to provide our services. Your data may be shared with internal and external parties
               for the purpose of improving and maintaining our API client. We ensure all external parties comply with
               our strict data protection guidelines.
             </p>
           </li>
           <li>
-            <h3 class="text-h5">Data Collection Methods</h3>
-            <p class="pt-2 pb-4">
+            <h3 class="text-xl font-medium mb-2">Data Collection Methods</h3>
+            <p class="py-2 mb-4">
               Data is collected through various means such as direct input, cookies, and other tracking technologies.
               This helps us to understand user preferences and provide better service.
             </p>
           </li>
           <li>
-            <h3 class="text-h5">Contact Information</h3>
-            <p class="pt-2 pb-4">
+            <h3 class="text-xl font-medium mb-2">Contact Information</h3>
+            <p class="py-2 mb-4">
               Contact Information If you have any questions or concerns about this privacy policy, please contact us at
-              <a href="mailto:classicminidiy@gmail.com" target="_blank">classicminidiy@gmail.com</a>.
+              <a class="link link-primary" href="mailto:classicminidiy@gmail.com" target="_blank"
+                >classicminidiy@gmail.com</a
+              >.
             </p>
           </li>
         </ol>
-      </v-col>
-    </v-row>
-  </v-container>
+      </div>
+    </div>
+  </div>
 </template>
