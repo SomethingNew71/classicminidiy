0dc2914b6948c438b302af46f26973d823394d83
Cole Gentry <peapod2007@gmail.com>
2025-05-22 10:14:33 -0400

    Migrates torque page, footer, patreon card
    Signed-off-by: Cole Gentry <peapod2007@gmail.com>

--- components/PatreonCard.vue
+++ components/PatreonCard.vue
@@ -15,18 +15,21 @@
 <template>
   <div>
     <template v-if="size === 'large'">
-      <div class="columns">
-        <div class="column is-3 avatar-container">
+      <div class="grid grid-cols-12 gap-4">
+        <div class="col-span-3 avatar-container">
           <nuxt-img
             format="webp"
             loading="lazy"
             src="https://classicminidiy.s3.amazonaws.com/misc/avatar.png"
             alt="Image of my car on jack stands"
+            class="w-full rounded-lg"
           />
         </div>
-        <div class="column is-9">
-          <h3 class="fancy-font-book-oblique is-size-5"><i class="fad fa-hands-heart"></i> SUPPORT THE SITE</h3>
-          <p class="is-size-5 pt-5">
+        <div class="col-span-9">
+          <h3 class="fancy-font-book-oblique text-lg font-semibold">
+            <i class="fad fa-hands-heart"></i> SUPPORT THE SITE
+          </h3>
+          <p class="text-lg py-5">
             Classic Mini DIY is
             <strong>completely free resource</strong> supported by our viewers. If you are interested in helping to keep
             the channel alive, consider supporting on Patreon.
@@ -34,17 +37,15 @@
           <p class="pt-3">
             <strong>Membership comes with tons of free perks and benefits!</strong>
           </p>
-          <p>
+          <p class="mt-4">
             <a
-              class="mt-4 button is-medium is-patreon"
+              class="btn btn-primary gap-2"
               rel="noopener"
               href="https://patreon.com/classicminidiy"
               target="_blank"
               @click="trackStuff(TRACKING_EVENTS.PATREON, path)"
             >
-              <span class="icon">
-                <i class="fab fa-patreon" />
-              </span>
+              <i class="fab fa-patreon"></i>
               <span>Contribute Today</span>
             </a>
           </p>
@@ -52,12 +53,14 @@
       </div>
     </template>
     <template v-if="size === 'small'">
-      <div class="columns is-multiline">
-        <div class="column is-12">
-          <h3 class="fancy-font-book-oblique is-size-5 pt-2"><i class="fad fa-hands-heart"></i> SUPPORT THE SITE</h3>
+      <div class="grid grid-cols-1 gap-2">
+        <div>
+          <h3 class="fancy-font-book-oblique text-lg font-semibold pt-2">
+            <i class="fad fa-hands-heart"></i> SUPPORT THE SITE
+          </h3>
         </div>
-        <div class="column is-12">
-          <p class="is-size-6">
+        <div>
+          <p class="text-sm">
             Classic Mini DIY is
             <strong>completely free resource</strong> supported by our viewers. If you are interested in helping to keep
             the channel alive, consider supporting on Patreon.
@@ -66,15 +69,13 @@
             <strong>Membership comes with tons of free perks and benefits!</strong>
           </p>
           <a
-            class="mt-4 button is-patreon"
+            class="btn btn-primary btn-sm gap-2 mt-4"
             rel="noopener"
             href="https://patreon.com/classicminidiy"
             target="_blank"
             @click="trackStuff(TRACKING_EVENTS.PATREON, path)"
           >
-            <span class="icon">
-              <i class="fab fa-patreon" />
-            </span>
+            <i class="fab fa-patreon"></i>
             <span>Contribute Today</span>
           </a>
         </div>
@@ -82,3 +83,16 @@
     </template>
   </div>
 </template>
+
+<style scoped>
+  .btn-primary {
+    background-color: #f96854; /* Patreon color */
+    border-color: #f96854;
+    color: white;
+  }
+
+  .btn-primary:hover {
+    background-color: #e55b4d;
+    border-color: #e55b4d;
+  }
+</style>
