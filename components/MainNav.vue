fb3b59245b2fd8cc3a6247541c6ffa2796386266
Cole Gentry <peapod2007@gmail.com>
2025-05-22 11:41:29 -0400

    More migration of various pages and components
    Signed-off-by: Cole Gentry <peapod2007@gmail.com>

--- components/MainNav.vue
+++ components/MainNav.vue
@@ -1,50 +1,15 @@
 <script lang="ts" setup>
-  import { ArchiveItems, ToolboxItems } from '~/data/models/generic';
+  const route = useRoute();
+
+  // Function to check if a route is active
+  const isActive = (path: string): boolean => {
+    return route.path === path || route.path.startsWith(path + '/');
+  };
 </script>
 
 <template>
   <div class="navbar bg-base-100 py-3">
     <div class="navbar-start">
-      <div class="dropdown">
-        <div tabindex="0" role="button" class="btn btn-ghost lg:hidden">
-          <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h8m-8 6h16" />
-          </svg>
-        </div>
-        <ul tabindex="0" class="menu menu-sm dropdown-content bg-base-100 rounded-box z-[1] mt-3 w-52 p-2 shadow">
-          <li>
-            <a href="https://classicminidiy.substack.com/" target="_blank">
-              <span><i class="fad fa-pencil"></i></span> Blog</a
-            >
-          </li>
-          <li>
-            <nuxt-link to="/maps">
-              <span><i class="fad fa-computer-classic"></i></span> Maps</nuxt-link
-            >
-          </li>
-          <li>
-            <a href="https://classicminidiy.substack.com/" target="_blank">
-              <span><i class="fad fa-store"></i></span> Store</a
-            >
-          </li>
-          <li>
-            <p>Archive</p>
-            <ul class="p-2">
-              <li v-for="item in ArchiveItems">
-                <nuxt-link :to="item.to"> <span v-html="item.iconHtml"></span> {{ item.title }} </nuxt-link>
-              </li>
-            </ul>
-          </li>
-          <li>
-            <p>Parent</p>
-            <ul class="p-2">
-              <li v-for="item in ToolboxItems">
-                <nuxt-link :to="item.to"> <span v-html="item.iconHtml"></span> {{ item.title }} </nuxt-link>
-              </li>
-            </ul>
-          </li>
-        </ul>
-      </div>
       <NuxtLink :to="'/'" class="d-inline-block">
         <nuxt-img
           alt="Classic Mini DIY Logo"
@@ -54,14 +19,14 @@
       </NuxtLink>
     </div>
     <div class="navbar-end hidden lg:flex">
-      <ul class="menu menu-horizontal px-1">
+      <ul class="menu menu-horizontal px-1 text-base">
         <li>
           <a href="https://classicminidiy.substack.com/" target="_blank">
             <span><i class="fad fa-pencil"></i></span> Blog</a
           >
         </li>
         <li>
-          <nuxt-link to="/maps">
+          <nuxt-link to="/maps" :class="{ active: isActive('/maps') }">
             <span><i class="fad fa-computer-classic"></i></span> Maps</nuxt-link
           >
         </li>
@@ -71,27 +36,53 @@
           >
         </li>
         <li>
-          <details>
-            <summary><i class="fad fa-books"></i>Archive</summary>
-            <ul class="p-2">
-              <li v-for="item in ArchiveItems" class="w-40 z-50">
-                <nuxt-link :to="item.to"> <span v-html="item.iconHtml"></span> {{ item.title }} </nuxt-link>
-              </li>
-            </ul>
-          </details>
+          <nuxt-link to="/archive" :class="{ active: isActive('/archive') }">
+            <span><i class="fad fa-books"></i></span> Archive</nuxt-link
+          >
         </li>
         <li>
-          <details>
-            <summary><i class="fad fa-toolbox"></i>Toolbox</summary>
-            <ul class="p-2 z-100">
-              <li v-for="item in ToolboxItems" class="w-40 z-100">
-                <nuxt-link :to="item.to"> <span v-html="item.iconHtml"></span> {{ item.title }} </nuxt-link>
-              </li>
-            </ul>
-          </details>
+          <nuxt-link to="/technical" :class="{ active: isActive('/technical') }">
+            <span><i class="fad fa-toolbox"></i></span> Toolbox</nuxt-link
+          >
         </li>
       </ul>
       <a class="btn donate" href="https://patreon.com/classicminidiy"> <i class="fab fa-patreon"></i>Join CMDIY</a>
+    </div>
+    <div class="navbar-end lg:hidden">
+      <div class="dropdown dropdown-end">
+        <div tabindex="0" role="button" class="btn btn-ghost lg:hidden">
+          <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h8m-8 6h16" />
+          </svg>
+        </div>
+        <ul tabindex="0" class="menu dropdown-content bg-base-100 rounded-box z-[1] mt-3 w-52 p-2 shadow">
+          <li>
+            <a href="https://classicminidiy.substack.com/" target="_blank">
+              <span><i class="fad fa-pencil"></i></span> Blog</a
+            >
+          </li>
+          <li>
+            <nuxt-link to="/maps" :class="{ active: isActive('/maps') }">
+              <span><i class="fad fa-computer-classic"></i></span> Maps</nuxt-link
+            >
+          </li>
+          <li>
+            <a href="https://classicminidiy.substack.com/" target="_blank">
+              <span><i class="fad fa-store"></i></span> Store</a
+            >
+          </li>
+          <li>
+            <nuxt-link to="/archive" :class="{ active: isActive('/archive') }">
+              <span><i class="fad fa-books"></i></span> Archive</nuxt-link
+            >
+          </li>
+          <li>
+            <nuxt-link to="/technical" :class="{ active: isActive('/technical') }">
+              <span><i class="fad fa-toolbox"></i></span> Toolbox</nuxt-link
+            >
+          </li>
+        </ul>
+      </div>
     </div>
   </div>
 </template>
