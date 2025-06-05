d4135fa4db72bc49fceb8f3af57707551445c2f8
Cole Gentry <peapod2007@gmail.com>
2025-06-04 21:10:07 -0400

    Migrates more pages to daisyui
    Signed-off-by: Cole Gentry <peapod2007@gmail.com>

--- pages/technical/parts.vue
+++ pages/technical/parts.vue
@@ -1,13 +1,35 @@
 <script lang="ts" setup>
+  import { ref } from 'vue';
   import { BREADCRUMB_VERSIONS, HERO_TYPES } from '~/data/models/generic';
-  const { data: tables } = await useFetch('/api/parts');
-  const searchValue = ref('');
-  const panels = ref(['Air Filters', 'Oil Filters', 'Alternators']);
 
-  const tableHeaders: any[] = [
-    { title: 'Brand', key: 'brand', align: 'start' },
-    { title: 'Part Number', key: 'part', align: 'start' },
-    { title: 'Usage Info', key: 'info', align: 'start' },
+  interface PartItem {
+    brand: string;
+    part: string;
+    info?: string;
+    [key: string]: any; // For any additional properties
+  }
+
+  interface PartsTable {
+    title: string;
+    items: PartItem[];
+  }
+
+  const { data: tables } = await useFetch<Record<string, PartsTable>>('/api/parts');
+  const searchValue = ref('');
+  const activePanels = ref<Record<string, boolean>>({
+    'Air Filters': true,
+    'Oil Filters': true,
+    Alternators: true,
+  });
+
+  const togglePanel = (panel: string) => {
+    activePanels.value[panel] = !activePanels.value[panel];
+  };
+
+  const tableHeaders = [
+    { title: 'Brand', key: 'brand' },
+    { title: 'Part Number', key: 'part' },
+    { title: 'Usage Info', key: 'info' },
   ];
 
   useHead({
@@ -20,6 +42,7 @@
       },
     ],
   });
+
   useSeoMeta({
     ogTitle: 'Tech - Parts Equivalency',
     ogDescription: 'A complete list of parts which can be found at local parts sellers',
@@ -31,55 +54,68 @@
 
 <template>
   <hero :navigation="true" :title="'Parts Equivalency'" :heroType="HERO_TYPES.TECH" />
-  <v-container>
-    <v-row>
-      <v-col cols="12">
-        <breadcrumb :version="BREADCRUMB_VERSIONS.TECH" page="Parts Equivalency"></breadcrumb>
-      </v-col>
-      <v-col cols="12">
-        <v-expansion-panels v-model="panels" variant="popout" multiple>
-          <v-expansion-panel v-for="(table, name, index) in tables" :key="`${name}-${index}`" :value="table.title">
-            <v-expansion-panel-title color="brand-green-3" expand-icon="fad fa-plus" collapse-icon="fad fa-plus">
-              {{ table.title }}
-            </v-expansion-panel-title>
-            <v-expansion-panel-text>
-              <v-row class="pb-5">
-                <v-spacer></v-spacer>
-                <v-text-field
-                  label="Search This Table"
+
+  <div class="container mx-auto px-4 py-6">
+    <div class="mb-6">
+      <breadcrumb :version="BREADCRUMB_VERSIONS.TECH" page="Parts Equivalency" />
+    </div>
+
+    <div class="space-y-4">
+      <div v-for="(table, name, index) in tables" :key="`${name}-${index}`" class="collapse collapse-arrow bg-base-200">
+        <input type="checkbox" :checked="activePanels[table.title]" @change="togglePanel(table.title)" />
+        <div class="collapse-title text-xl font-medium flex items-center justify-between">
+          {{ table.title }}
+        </div>
+        <div class="collapse-content">
+          <div class="py-4">
+            <div class="form-control w-full max-w-xs ml-auto mb-4">
+              <div class="relative">
+                <input
+                  type="text"
                   v-model="searchValue"
-                  placeholder="Crankshaft"
-                  append-inner-icon="fad fa-search"
-                  variant="underlined"
-                  class="pr-4 pt-2"
-                ></v-text-field>
-              </v-row>
-              <v-data-table
-                :headers="tableHeaders"
-                :items="table.items"
-                :density="'compact'"
-                :item-value="'name'"
-                items-per-page="10"
-                :search="searchValue"
-              >
-              </v-data-table>
-            </v-expansion-panel-text>
-          </v-expansion-panel>
-        </v-expansion-panels>
-      </v-col>
+                  placeholder="Search..."
+                  class="input input-bordered w-full pr-10"
+                />
+                <span class="absolute right-3 top-1/2 -translate-y-1/2">
+                  <i class="fas fa-search text-gray-400"></i>
+                </span>
+              </div>
+            </div>
 
-      <v-col cols="12" md="10" offset-md="1">
-        <div class="divider">Support</div>
-      </v-col>
-      <v-col cols="12">
-        <patreon-card size="large" />
-      </v-col>
-    </v-row>
-  </v-container>
+            <div class="overflow-x-auto">
+              <table class="table table-zebra w-full">
+                <thead>
+                  <tr>
+                    <th v-for="header in tableHeaders" :key="header.key">
+                      {{ header.title }}
+                    </th>
+                  </tr>
+                </thead>
+                <tbody>
+                  <tr
+                    v-for="(item, itemIndex) in table.items.filter((i) =>
+                      Object.values(i).some((val) => String(val).toLowerCase().includes(searchValue.toLowerCase()))
+                    )"
+                    :key="itemIndex"
+                  >
+                    <td>{{ item.brand }}</td>
+                    <td>{{ item.part }}</td>
+                    <td>{{ item.info }}</td>
+                  </tr>
+                  <tr v-if="!table.items.length">
+                    <td :colspan="tableHeaders.length" class="text-center py-4">No items found</td>
+                  </tr>
+                </tbody>
+              </table>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+
+    <div class="divider my-12">Support</div>
+    <div class="mb-8">
+      <patreon-card size="large" />
+    </div>
+  </div>
 </template>
-
-<style lang="scss" scoped>
-  .card-header {
-    background-color: whitesmoke;
-  }
-</style>
